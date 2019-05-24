ba8003d3695734a6316625e8265241a7fc5babc2
Adrian Adames <adrianadames@gmail.com>
2019-05-23 20:23:40 -0400

    Added problem statement as a comment to graph.js

---
+++ dataStructures/Graph/graph.js
@@ -0,0 +1,20 @@
+/*
+Data structures to use in implementation of graph data structure: 
+-Queue
+-Stack
+-Vertex
+-Graph
+
+Methods: 
+-addVertex
+-addEdge
+-addDirectedEdge
+-dftRecursive
+-dftStack
+-dfsRecursive
+-dfsStack
+-bft
+-bfs
+
+NOTE: There are different ways to implement the graph data structure. Implement another version.
+*/
