24bce3a9f2c1780824946e063f9112ed5110426a
Adrian Adames <adrianadames@gmail.com>
2019-05-23 20:22:42 -0400

    Added problem statement as a comment.

---
+++ dataStructures/linkedList/linkedList.js
@@ -0,0 +1,16 @@
+/*
+Methods: 
+-addToTail (replaces the tail with a new value that is passed in)
+-removeHead (deletes head)
+-addToHead (puts a new head node in front of the current head)
+-contains (should search through the linked list and return true 
+
+if a matching value is found)
+-getMax (returns the maximum value in the linked list)
+-containsSequence (?)
+
+Apply knowledge:
+-Implement a stack with a singly linked list.
+-Implement a queue with a singly linked list.
+-Reverse a singly linked list
+*/
