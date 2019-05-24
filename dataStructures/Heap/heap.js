ecca0cd03288a6b1bcfd5551d71a1ef0d2989f45
Adrian Adames <adrianadames@gmail.com>
2019-05-23 20:41:09 -0400

    Added problem statement as a comment in heap.js.

---
+++ dataStructures/Heap/heap.js
@@ -0,0 +1,16 @@
+/*
+NOTES:
+-problem statement not quite complete. See chapters 6(Heapsort) and 9(Priority Queues) in Goodrich text. 
+
+Methods: 
+-insert
+-delete
+-getMax
+-getSize
+-_bubbleUP_
+-_siftDown_
+-_maxChild_
+
+Apply knowledge:
+Turn your max Heap into a min Heap or vice-versa.
+*/
