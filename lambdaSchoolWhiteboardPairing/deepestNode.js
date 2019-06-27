054a0f53b44cb4a52781354876213553d189e63d
adrian <adrianadames@gmail.com>
2019-06-27 17:34:48 -0400

    In progress. Solution not close to done.

--- lambdaSchoolWhiteboardPairing/deepestNode.js
+++ lambdaSchoolWhiteboardPairing/deepestNode.js
@@ -10,3 +10,49 @@ Given the root of a binary tree, return a deepest node. For example, in the foll
 d
 
 */
+
+class BinaryTree {
+    constructor(value, left  = null, right  = null){
+        this.value = value,
+        this.left = left,
+        this.right = right
+    }
+
+    deepestNode = () => {
+        let deepestNode = this;
+        let deepestLeftNode, deepestRightNode;
+        let depth, right_depth,left_depth=0;
+
+        while (this.left !== null && this.right !== null) {
+            if (this.left === null && this.right === null) {
+                return (this, depth)
+            } else if (this.left && this.right === null) {
+                left_depth+=1;
+                deepestLeftNode = this.left;
+            } else if (this.left === null && this.right) {
+                right_depth+=1;
+                deepestRightNode = this.right;
+            } else {
+                left_depth+=1;
+                right_depth+=1;
+                deepestLeftNode = this.left;
+                deepestRightNode = this.right;
+            }
+            this.deepestNode()
+        }
+
+        if (left_depth>right_depth) {
+            return (deepestLeftNode, left_depth)
+        } else if (right_depth>= left_depth) {
+            return (deepestRightNode, right_depth)
+        }
+    }
+}
+
+let bt1=new BinaryTree(5)
+
+bt1.right = new BinaryTree(7);
+bt1.left = new BinaryTree(6);
+
+console.log(bt1.deepestNode())
+
