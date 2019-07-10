8ac327323c196a7a5c0c45cc42a96f18339f9980
adrian <adrianadames@gmail.com>
2019-07-10 18:03:16 -0400

    Done with problem 002.

--- dailyCodingProblems/problem002.js
+++ dailyCodingProblems/problem002.js
@@ -12,3 +12,49 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be
 Follow-up: what if you can't use division?
 
 */
+
+
+// // USING DIVISION 
+
+// productOfAllExceptIndex = (arr) => {
+//     let productOfAll = 1; 
+//     let outputArr = [];
+
+//     for (let i = 0; i< arr.length;i++) {
+//         productOfAll = productOfAll*arr[i]
+//     }
+
+//     for (let i = 0; i< arr.length;i++) {
+//         outputArr.push(productOfAll/arr[i])
+//     }
+//     return outputArr
+// }
+
+
+// NOT USING DIVISION 
+
+productOfAllExceptIndex = (arr) => {
+    let productOfAll = 1; 
+    let outputArr = arr.slice();
+    outputArr.fill(1);
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (i === j) {
+                continue
+            } 
+            if (i !== j) {
+                outputArr[i] = outputArr[i]*arr[j]
+            }
+        }
+    }
+    return outputArr
+}
+
+
+
+const arr1 = [1,2,3,4,5]
+const arr2 = [3,2,1]
+console.log(productOfAllExceptIndex(arr1))
+console.log(productOfAllExceptIndex(arr2))
+
