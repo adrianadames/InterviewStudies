db79274994c5a3a0ca8b5eca78d9087b1aa2fb30
adrian <adrianadames@gmail.com>
2019-07-11 18:14:36 -0400

    Copy pasted problem statement.

---
+++ checkPalindrome.js
@@ -0,0 +1,15 @@
+/*
+1) Check Palindrome: Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+checkPalindrome(inputString) = true;
+For inputString = "abac", the output should be
+checkPalindrome(inputString) = false;
+For inputString = "a", the output should be
+checkPalindrome(inputString) = true.
+*/
+
+// A palindrome is a word, phrase, or sequence that reads the same backward as forward. 
+
