a1cee1de4e5ba691edf2d8bb4aab8fafe451e4a5
adrian <adrianadames@gmail.com>
2019-10-01 23:38:45 -0400

    Graph data structure code in progress. Still need to do all the bft/dft methods.

--- dataStructures/Graph/graph.js
+++ dataStructures/Graph/graph.js
@@ -76,3 +76,64 @@ class Stack {
         }
     } 
 }
+
+
+
+//when we did it in python, we had the graph represented by a dictionary of all the vertices in the graph
+// the vertices had their own data structure. one of the vertices' properties was edges, 
+
+class Vertex {
+    constructor(vertex_id, value = null, color = "white") {
+        this.id = vertex_id;  //unique id for each vertex
+        this.value = value;
+        this.color = color;
+        this.edges = new Set();
+    }
+}
+
+class Graph {
+    // Graph to be represented as an object of vertices mapping each vertex to its edges
+    constructor(vertices = {}) {
+        this.vertices = vertices;
+    }
+    
+    addVertex(vertex_id) {
+        this.vertices[vertex_id] = new Vertex(vertex_id);
+    }
+
+    addEdge(v1_id, v2_id) {
+        if (this.vertices[v1_id] && this.vertices[v2_id]) {
+            this.vertices[v1_id].edges.push(v2_id);
+            this.vertices[v2_id].edges.push(v1_id);
+        } else {
+            return ReferenceError('v1 or v2 is not a vertex in this graph')
+        }
+    }
+   
+    addDirectedEdge(v1_id, v2_id) {
+        if (this.vertices[v1_id] && this.vertices[v2_id]) {
+            this.vertices[v1_id].edges.push(v2_id);
+        } else {
+            return ReferenceError('v1 or v2 is not a vertex in this graph')
+        }
+    }
+
+
+
+    dftRecursive() {}
+    dftStack() {}
+    dfsRecursive() {}
+    dfsStack() {}
+    bft() {}
+    bfs() {}
+
+}
+
+
+let v1 = new Vertex(3);
+
+let g1 = new Graph();
+
+console.log(v1,g1)
+
+console.log(g1.addEdge(v1))
