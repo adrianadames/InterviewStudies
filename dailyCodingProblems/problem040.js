c54db0101be686287dd2d4f17714f44c0d77a636
adrian <adrianadames@gmail.com>
2019-10-25 13:53:39 -0400

    Didn't get solution. Moving on for now.

--- dailyCodingProblems/problem040.js
+++ dailyCodingProblems/problem040.js
@@ -10,3 +10,33 @@ For example, given [6, 1, 3, 3, 3, 6, 6], return 1. Given
 
 Do this in O(N) time and O(1) space.
 */
+
+// // FAIL. Solution at the bottom. 
+
+// function findNonDuplicate(arr) {
+//     let counts = {};
+//     let appearsOnce, appearTwice;
+//     for (let i=0; i < arr.length; i++) {
+//         if (!counts[arr[i]]) {
+//             counts[arr[i]]
+//         }
+        
+//     }
+//     // console.log(counts);
+//     // return Object.keys(counts)[0];
+// }
+
+// // console.log(findNonDuplicate([6, 1, 3, 3, 3, 6, 6]));
+// console.log(findNonDuplicate([13, 19, 13, 13]));
+
+
+
+// function findElement(arr) {
+//   var ones = 0;
+//   var twos = 0;
+//   for (var i = 0; i < arr.length; i++) {
+//     ones = (ones ^ arr[i]) & ~twos;
+//     twos = (twos ^ arr[i]) & ~ones;
+//   }
+//   return ones;
+// }
