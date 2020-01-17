ae25f24bedb6866f1d3d7f6ac2d79d6b6338b9aa
adrian <adrianadames@gmail.com>
2020-01-16 19:29:11 -0500

    Solved find second largest node in bst problem (#36).

--- dailyCodingProblems/problem036.js
+++ dailyCodingProblems/problem036.js
@@ -23,3 +23,67 @@ Given the root to a binary search tree, find the second largest node in the tree
 // -if this largest node has a RHS subtree, set the secondLargest var to that,
 //  and set its RHS subtree as the largest
 // -if the current largest doesn't have a RHS, return secondLargest
+
+class BST {
+    constructor(val, left = null, right = null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    insert(val) {
+        let currentBSTRoot = this;
+        let newBSTRoot = new BST(val);
+
+        if (newBSTRoot.val >= currentBSTRoot.val) {
+            if (!this.right) {
+                this.right = newBSTRoot;
+            } else {
+                currentBSTRoot = this.right;
+                currentBSTRoot.insert(val)
+            }
+        } 
+        else if (newBSTRoot.val < currentBSTRoot.val) {
+                if (!this.left) {
+                    this.left = newBSTRoot;
+                } else {
+                    currentBSTRoot = this.left;
+                    currentBSTRoot.insert(val)
+                }
+        }  
+    }
+}
+
+
+// example tree
+let bst1 = new BST(10);
+// console.log(bst1.val)
+
+bst1.insert(20);
+// console.log(bst1.right.val)
+
+bst1.insert(25);
+// console.log(bst1.right.right.val)
+
+bst1.insert(13);
+// console.log(bst1.right.left.val)
+
+bst1.insert(26);
+ 
+
+
+function secondLargestVal(bst) {
+    if (!bst.right) {
+        return bst.left
+    }
+    let largest = bst.right;
+    let secondLargest = bst;
+
+    if (largest.right === null) {
+        return secondLargest.val
+    } else {
+        return secondLargestVal(largest)
+    }
+} 
+
+console.log(secondLargestVal(bst1))
