6d64bc04f270777cbdc4865256b0acd611d0ce5a
adrian <adrianadames@gmail.com>
2020-01-16 20:18:00 -0500

    Solved runningMedian problem (#33).

--- dailyCodingProblems/problem033.js
+++ dailyCodingProblems/problem033.js
@@ -16,3 +16,29 @@ For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should pri
 2
 2
 */
+
+function runningMedian(arr) {
+    let runningArr =[];
+    let runningArrMedian = [];
+    
+    while (arr.length > 0) {
+        runningArr.push(arr.shift());
+        runningArr.sort();
+
+        let middleIndex = (runningArr.length -1)/2;
+
+        if (runningArr.length % 2 === 0) {
+            runningArrMedian.push((runningArr[Math.floor(middleIndex)]+runningArr[Math.ceil(middleIndex)])/2);
+            // console.log('runningArrMedian: ', runningArrMedian);
+        } else {
+            runningArrMedian.push(runningArr[middleIndex]);
+            // console.log('runningArrMedian: ', runningArrMedian);
+        }
+    }
+    return runningArrMedian
+}
+
+
+let arr1 = [2, 1, 5, 7, 2, 0, 5];
+
+console.log(runningMedian(arr1));
