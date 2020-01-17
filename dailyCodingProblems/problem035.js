7fcfe2db00b2435f760b8bd19214031e17ca1d88
adrian <adrianadames@gmail.com>
2020-01-16 19:56:31 -0500

    Solved orderIntoRGBs problem (problem #35).

--- dailyCodingProblems/problem035.js
+++ dailyCodingProblems/problem035.js
@@ -14,3 +14,37 @@ become ['R', 'R', 'R', 'G', 'G', 'B', 'B'].
 
 // first set start equal to the first index and then move the end pointer one by one from the end
 // once they match, we move the start index to the second index and then move the end pointer one by one
+
+
+function orderIntoRGBs(arr) {
+    let startIndex =-1;
+
+    let RGBVals = {
+        'R':0,
+        'G':1,
+        'B':2
+    };
+
+    while (startIndex !== arr.length - 1) {
+        startIndex += 1;
+        let endIndex = arr.length-1;
+
+        while (startIndex !== endIndex) {
+            let startVal = arr[startIndex];
+            let endVal = arr[endIndex];
+
+            if (RGBVals[startVal] > RGBVals[endVal]) {
+                arr[startIndex] = endVal;
+                arr[endIndex] = startVal;
+            }
+            endIndex -= 1;
+        }
+    }
+    
+    console.log(arr)
+    return arr
+}
+
+const arr1 = ['G', 'B', 'R', 'R', 'B', 'R', 'G'];
+
+orderIntoRGBs(arr1)
