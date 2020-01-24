7c4cc5d42b4129966879014557a30903e14bb389
adrian <adrianadames@gmail.com>
2020-01-23 21:43:55 -0500

    Tweaked problem statement by adding an additional thing to return.

--- dailyCodingProblems/problem026.js
+++ dailyCodingProblems/problem026.js
@@ -1,9 +1,13 @@
 /*
 This problem was asked by Google.
 
-Given a singly linked list and an integer k, remove the kth last element from the list. k is guaranteed to be smaller than the length of the list.
+Given a singly linked list and an integer k, remove the kth last element from 
+the list. k is guaranteed to be smaller than the length of the list.
 
 The list is very long, so making more than one pass is prohibitively expensive.
 
 Do this in constant space and in one pass.
+
+NOTE: I'm making a tweak to this problem. In addition to removing the kth to the last node, 
+also return what the kth to last node is. 
 */
