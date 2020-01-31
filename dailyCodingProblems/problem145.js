6203b96fcbf5255b420c7200547212083bffc60b
adrian <adrianadames@gmail.com>
2020-01-30 21:12:05 -0500

    Wrote up solution in js. I still don't have a good understanding of it.

--- dailyCodingProblems/problem145.js
+++ dailyCodingProblems/problem145.js
@@ -16,58 +16,31 @@ class Node {
     }
 }
 
+function swapTwoHelper(node) {
 
-function swapEveryTwoNodes(head) {
-    if (!head) {
-        return null
+    let first, second, tail;
+    if (!node || !node.next) {
+        return node
     }
 
-    let curr = head;
-    let next = head.next;
-    let newHead = next;
+    first = node;
+    second = node.next;
+    tail = swapTwoHelper(second.next)
 
-    let i = 0;
-    let j = 0;
+    second.next = first;
+    first.next = tail;
 
-    while ( i < 2) {
-        // current node now points two nodes down
-        curr.next = next.next;
-
-        // node that curr node was originally pointing to, now points to the original node 
-        next.next = curr;
-
-        // curr node still refers to the original node we started with
-        // next node now refers to the node that the original pointer is pointing to now (the node two nodes down from where we started)
-        curr = curr;
-        next = curr.next;
-
-
-        //current node now points two nodes down
-        curr.next = next.next;
-
-
-        while (j < 2) {
-            curr = next;
-            next = curr.next;
-            
-            curr.next = next.next;
-            next.next = next.next.next;
-    
-            curr = curr.next;
-            curr.next = next;
-            
-            j+=1
-        }
-        i+=1
-
-        
-    }
-
-    return newHead
-    
-
-
+    return second
 }
+
+function swapTwo(head) {
+    let dummyHead = new Node(null);
+    dummyHead.next = head;
+
+    return swapTwoHelper(head)
+}
+
+
 let n1 = new Node(1);
 let n2 = new Node(2);
 let n3 = new Node(3);
@@ -76,6 +49,7 @@ let n5 = new Node(5);
 let n6 = new Node(6);
 let n7 = new Node(7);
 let n8 = new Node(8);
+let n9 = new Node(9);
 
 // console.log(n1, n2)
 
@@ -85,13 +59,14 @@ n3.next = n4;
 n4.next = n5;
 n5.next = n6;
 n6.next = n7;
-n7.next = n8
+n7.next = n8;
+n8.next = n9;
 // console.log(swapEveryTwoNodes(n1))
 
 
-let swappedLinkedList = swapEveryTwoNodes(n1)
+let swappedLinkedList = swapTwo(n1)
 
-console.log(swappedLinkedList.next)
+console.log(swappedLinkedList.next.next.next)
 
 
 
@@ -110,35 +85,46 @@ console.log(swappedLinkedList.next)
 
 
 
-
 // function swapEveryTwoNodes(head) {
-//     let count = 0;
-//     let currentNode = head;
-//     let newHead = currentNode.next;
-//     let temp1, temp2;
-
-//     while (count < 1) {
-//         if (count % 2 === 0) {
-//             temp1 = currentNode;
-//             console.log('temp1: ', temp1);
-//             temp2 = currentNode.next;
-//             console.log('temp2: ', temp2);
-//             console.log('temp2.next: ', temp2.next);
-            
-//             currentNode = temp2;
-//             currentNode.next = temp1;
-//             console.log('currentNode.next: ', currentNode.next);
-//             currentNode.next.next = temp2.next;
-//             console.log('currentNode.next.next: ', currentNode.next.next);
-
-//             count +=1;
-//         } else {
-//             count +=1;
-//             currentNode = currentNode.next
-//         }
+//     if (!head) {
+//         return null
 //     }
 
+//     let curr = head;
+//     let next = head.next;
+//     let newHead = next;
+
+//     let i = 0;
+//     let j = 0;
+
+//     while ( i < 2) {
+//         // current node now points two nodes down
+//         curr.next = next.next;
+
+//         // node that curr node was originally pointing to, now points to the original node 
+//         next.next = curr;
+
+//         // curr node still refers to the original node we started with
+//         // next node now refers to the node that the original pointer is pointing to now (the node two nodes down from where we started)
+//         curr = curr;
+//         next = curr.next;
+
+//         //current node now points two nodes down
+//         curr.next = next.next;
+
+//         while (j < 2) {
+//             curr = next;
+//             next = curr.next;
+            
+//             curr.next = next.next;
+//             next.next = next.next.next;
+    
+//             curr = curr.next;
+//             curr.next = next;
+            
+//             j+=1
+//         }
+//         i+=1
+//     }
 //     return newHead
 // }
-
-// console.log(swapEveryTwoNodes(n1))
