4d47b18bf947ba707dd179f22e4a49d2c77137fb
adrian <adrianadames@gmail.com>
2020-04-18 13:24:56 -0400

    Copy pasted problems my friend asked me to help them with.

---
+++ misclProblems/characterCounter.py
@@ -0,0 +1,6 @@
+"""
+Character Counter
+❖ Write a function that accepts two arguments – a string and an single character
+❖ Have your function search the string for all occurrences of that character
+❖ Return the number of times the character was found in the supplied string
+"""
---
+++ misclProblems/usernameGeneration.py
@@ -0,0 +1,16 @@
+"""
+Programming Challenge: Username Generation
+❖ You just accepted a position at NYU’s ITS department and your first task is to
+write a
+program that generates student Net IDs for all incoming freshmen
+❖ Net IDs are generated as follows:
+❖ The first two characters of a student’s first name
+❖ The first two characters of a student’s last name
+❖ The last three characters of a student’s N#
+❖ Write a program that asks the user for these three pieces of information (first
+name,
+last name and N#) and generate their Net ID.
+❖ Note that if a student’s first name or last name is less than 2 characters then
+you
+should use the entire first name or last name.
+"""
