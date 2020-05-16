856a5aae9459d70e2607db1c1e7b27648177fc0c
adrian <adrianadames@gmail.com>
2020-05-16 14:10:54 -0400

    Got working solution. But time complexity is too high.

--- grokkingCodeInterview/3-twoPointers/2-removeDuplicates.js
+++ grokkingCodeInterview/3-twoPointers/2-removeDuplicates.js
@@ -17,3 +17,33 @@ Input: [2, 2, 2, 11]
 Output: 2
 Explanation: The first two elements after removing the duplicates will be [2, 11].
 */
+
+function removeDuplicates(arr) {
+    let pointerA = 0;
+    let pointerB = 1; 
+
+    while (pointerA < arr.length  && pointerB < arr.length ) {
+        if (arr[pointerA] === arr[pointerB]) {
+            arr.splice(Math.max(pointerA, pointerB),1);
+        } else {
+            if (pointerA < pointerB) {
+                pointerA = pointerB + 1;
+            } else {
+                pointerB = pointerA + 1
+            }
+        }
+    }
+    return arr.length
+}
+
+console.log(removeDuplicates([2, 3, 3, 3, 6, 9, 9]));
+console.log(removeDuplicates([2, 2, 2, 11]));
+
+// time big 0 
+// - not sure about this because not sure what the time complexity of the splice function is
+// - i think it would be very large. imagine every number being the same. all the values
+//   to the right of something being deleted would need to be shifted over. that's O(n)
+// - and then you would need to perform that splice function n times, meaning that 
+//   the overall big O would be O(n^2)
+
+// space big 0:  O(1) 
