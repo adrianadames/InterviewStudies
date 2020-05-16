1009c684d9f9792f85f256a16a11b82c4e06ff98
adrian <adrianadames@gmail.com>
2020-05-16 13:30:14 -0400

    Had started wrong problem just now. Undid the changes. Starting the correct problem now.

--- grokkingCodeInterview/3-twoPointers/5-tripletSumToZero.js
+++ grokkingCodeInterview/3-twoPointers/5-tripletSumToZero.js
@@ -1,7 +1,7 @@
 /*
 (medium)
 
-Problem Statement: 
+Problem Statement #
 Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
 
 Example 1:
@@ -9,7 +9,6 @@ Example 1:
 Input: [-3, 0, 1, 2, -1, 1, -2]
 Output: [-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]
 Explanation: There are four unique triplets whose sum is equal to zero.
-
 Example 2:
 
 Input: [-5, 2, -1, -2, 3]
