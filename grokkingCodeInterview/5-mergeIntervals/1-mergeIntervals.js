26d48bcf72e0e82d3ee1ff9004bf46d00c184b38
adrian <adrianadames@gmail.com>
2020-08-09 10:42:53 -0400

    Started mergeIntervals problem.

--- grokkingCodeInterview/5-mergeIntervals/1-mergeIntervals.js
+++ grokkingCodeInterview/5-mergeIntervals/1-mergeIntervals.js
@@ -21,3 +21,90 @@ Intervals: [[1,4], [2,6], [3,5]]
 Output: [[1,6]]
 Explanation: Since all the given intervals overlap, we merged them into one.
 */
+
+// function mergeIntervals(arr) {
+//     let intervalsArr = arr;
+
+//     for (let i = 0; i< intervalsArr.length; i++) {
+//         let intervalA = intervalsArr[i];
+//         let intervalB = intervalsArr[i+1];
+
+//         // console.log('intervalB = ', intervalB)
+
+//         if (intervalA[0] <= intervalB[0]) {
+//             //check if the end of interval A ends after the start of interval B
+
+//             if (intervalA[1] >= intervalB[1]) {
+//                 intervalsArr.splice(intervalsArr.indexOf(intervalB), 1);
+//             } else {
+//                 intervalA[1] = intervalB[1];
+//                 intervalsArr.splice(intervalsArr.indexOf(intervalB), 1);
+//             }
+//         }
+
+//         if (intervalB[0] < intervalA[0]) {
+//             //check if the end of interval B ends after the start of interval A
+
+//             if (intervalB[1] >= intervalA[1]) {
+//                 intervalsArr.splice(intervalsArr.indexOf(intervalA), 1);
+//             } else {
+//                 intervalB[1] = intervalA[1];
+//                 intervalsArr.splice(intervalsArr.indexOf(intervalA), 1);
+//             }
+//         }
+
+//         console.log('intervalsArr = ', intervalsArr);
+//     }
+// }
+
+function mergeIntervals(arr) {
+    let intervalsArr = arr;
+
+    if (intervalsArr.length < 2) {
+        return intervalsArr
+    } 
+
+    for (let i = 0; i < intervalsArr.length; i++) {
+        let intervalA = intervalsArr[0]; 
+
+        for (let j = 1; j < intervalsArr.length; j++) {
+            let intervalB = intervalsArr[j]; 
+
+
+
+        }
+    }
+
+    for (let i = 0; i< intervalsArr.length; i++) {
+        let intervalA = intervalsArr[i];
+        let intervalB = intervalsArr[i+1];
+
+        // console.log('intervalB = ', intervalB)
+
+        if (intervalA[0] <= intervalB[0]) {
+            //check if the end of interval A ends after the start of interval B
+
+            if (intervalA[1] >= intervalB[1]) {
+                intervalsArr.splice(intervalsArr.indexOf(intervalB), 1);
+            } else {
+                intervalA[1] = intervalB[1];
+                intervalsArr.splice(intervalsArr.indexOf(intervalB), 1);
+            }
+        }
+
+        if (intervalB[0] < intervalA[0]) {
+            //check if the end of interval B ends after the start of interval A
+
+            if (intervalB[1] >= intervalA[1]) {
+                intervalsArr.splice(intervalsArr.indexOf(intervalA), 1);
+            } else {
+                intervalB[1] = intervalA[1];
+                intervalsArr.splice(intervalsArr.indexOf(intervalA), 1);
+            }
+        }
+
+        console.log('intervalsArr = ', intervalsArr);
+    }
+}
+
+mergeIntervals([[2,6], [1,4]])
