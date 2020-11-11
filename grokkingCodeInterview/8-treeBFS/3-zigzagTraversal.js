8b2bce1f9472877550bb76c8a25e07ecee7261e0
adrian <adrianadames@gmail.com>
2020-11-10 22:10:52 -0500

    Copy pasted problem statements that I missed yesterday.

--- grokkingCodeInterview/8-treeBFS/3-zigzagTraversal.js
+++ grokkingCodeInterview/8-treeBFS/3-zigzagTraversal.js
@@ -4,6 +4,12 @@
 
 Problem Statement
 
+Given a binary tree, populate an array to represent its zigzag 
+level order traversal. You should populate the values of all 
+nodes of the first level from left to right, then right to left 
+for the next level and keep alternating in the same manner for 
+the following levels.
+
 Ex1
 
        1
--- grokkingCodeInterview/8-treeBFS/4-levelAverages.js
+++ grokkingCodeInterview/8-treeBFS/4-levelAverages.js
@@ -4,6 +4,9 @@
 
 Problem Statement
 
+Given a binary tree, populate an array to represent the 
+averages of all of its levels.
+
 Ex1
 
        1
