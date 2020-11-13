a8a20f91e3892796b5e89550afcbd6e147ed9700
adrian <adrianadames@gmail.com>
2020-11-13 17:15:19 -0500

    Made data structure for binary tree. Built the binary trees in the examples. Will apply level order traversal algorithm to these to test.

--- grokkingCodeInterview/8-treeBFS/1-levelOrderTraversal.js
+++ grokkingCodeInterview/8-treeBFS/1-levelOrderTraversal.js
@@ -27,3 +27,30 @@ Level Order Traversal:
 [[12],[7,1],[9,10,5]]
 
 */
+
+class BinaryTree {
+  constructor(value, left = null, right = null) {
+    this.value = value; 
+    this.left = left; 
+    this.right = right; 
+  }
+}
+
+// ex 1 BT
+let bt1 = new BinaryTree(1);
+bt1.left = new BinaryTree(2);
+bt1.right = new BinaryTree(3);
+bt1.left.left = new BinaryTree(4);
+bt1.left.right = new BinaryTree(5);
+bt1.right.left = new BinaryTree(6);
+bt1.right.right = new BinaryTree(7);
+console.log('bt1: ', bt1);
+
+// ex 2 BT
+let bt2 = new BinaryTree(12);
+bt2.left = new BinaryTree(7);
+bt2.right = new BinaryTree(1);
+bt2.left.left = new BinaryTree(9);
+bt2.right.left = new BinaryTree(10);
+bt2.right.right = new BinaryTree(5);
+console.log('bt2: ', bt2);
