79fc27c2d9b88b42e99b2419533e95d07fdce6e4
adrian <adrianadames@gmail.com>
2020-11-17 16:42:37 -0500

    Copy pasted linked list based queue implementation I wrote previously.

--- grokkingCodeInterview/8-treeBFS/2-reverseLevelOrderTraversal.js
+++ grokkingCodeInterview/8-treeBFS/2-reverseLevelOrderTraversal.js
@@ -68,6 +68,85 @@ class Queue {
 // -another way to do this would be to use a stack (i.e. levelOrderStack) to store each levelArray, and then at the end 
 // pop out each element from the stack into the output array (i.e. levelOrderArr).  
 
+// LINKED LIST QUEUE IMPLEMENTATION
+class Node {
+    constructor(value = null, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null; 
+        this.tail = null; 
+        this.size = 0;
+    }
+
+    addToTail(nodeValue) {
+        if (this.size === 0) {
+            this.head = new Node(nodeValue);
+            this.size +=1;
+        } else if (this.size === 1) {
+            this.head.next = new Node(nodeValue);
+            this.tail = this.head.next;
+            this.size +=1; 
+        } else {
+            let oldTail = this.tail; 
+            let newTail = new Node(nodeValue);
+            oldTail.next = newTail;
+            this.tail = newTail;
+            this.size += 1;
+        }
+    }
+
+    removeHead() {
+        if (this.size === 0) {
+            return null
+        } else if (this.size === 1) {
+            let removedHead = this.head; 
+            this.head = null; 
+            this.tail = null;
+            this.size -= 1;
+            return removedHead; 
+        } else {
+            let removedHead = this.head; 
+            let newHead = this.head.next; 
+            this.head = newHead 
+            this.size -= 1;
+            return removedHead
+        }
+    }
+}
+
+class Queue {
+    constructor() {
+        this.items = new LinkedList();
+        this.size = this.items.size;
+    }
+
+    enqueue(value) {
+        if (value === null) {
+            return Error('The item passed into this function must be a non-null value.')
+        } else if (value === undefined) {
+            return Error('The item passed into the function is undefined.')
+        } else {
+            this.items.addToTail(value);
+            this.size = this.items.size;
+        }
+    }
+
+    dequeue(value) {
+        if (this.size < 1) {
+            return 'queue is empty'
+        } else {
+            this.size -= 1;
+            return this.items.removeHead();
+        }
+    }
+}
+
+// // REVERSE ARRAY HELPER FUNCTION
 // time complexity: O(n)
 // space complexity: O(1)
 function reverseArray(arr) {
