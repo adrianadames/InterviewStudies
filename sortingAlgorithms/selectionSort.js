f298c58757bbb34634db3b96aa80f5c329463d1d
adrian <adrianadames@gmail.com>
2022-03-09 20:46:13 -0500

    Done with selection sort.

--- sortingAlgorithms/selectionSort.js
+++ sortingAlgorithms/selectionSort.js
@@ -5,9 +5,64 @@
 // -Necaise 5.2.2
 // -Corbin Ex. 2.2-2 in Section 2.3 Exercises
 
-
 // runtime:O(n^2)  (best case = worst case = O(n^2))
-// spacetime: O(n)
+// spacetime: O(1)
+
+/*
+TD: selection sort for linked list. 
+*/
+
+/*
+-first compare 1st element and every number its right; switch if other number larger
+-at this point we know smallest number in array is first
+
+-then compare 2nd element and every number to its right; switch if other number larger
+-at this point we know 2nd smallest number in array is second
+
+-and so on
+
+ex: [2, 5, 1, 9, 6]
+
+first loop: n= 0
+sub-loops: 
+i=0
+[2, 5, 1, 9, 6]
+i=1
+[2, 5, 1, 9, 6]
+i=2
+[1, 5, 2, 9, 6]
+i=3
+[1, 5, 2, 9, 6]
+i=4
+[1, 5, 2, 9, 6]
+
+2nd loop: n= 1
+sub-loops: 
+i=1
+[1, 5, 2, 9, 6]
+i=2
+[1, 2, 5, 9, 6]
+i=3
+[1, 2, 5, 9, 6]
+i=4
+[1, 2, 5, 9, 6]
+
+3rd loop: n= 2
+sub-loops: 
+i=2
+[1, 2, 5, 9, 6]
+i=3
+[1, 2, 5, 9, 6]
+i= 4
+[1, 2, 5, 9, 6]
+
+4th loop: n= 3
+sub-loops: 
+i=3
+[1, 2, 5, 9, 6]
+i= 4
+[1, 2, 5, 6, 9]
+*/
 
 // using es6 destructuring assignment
 function selectionSort(arr) {
