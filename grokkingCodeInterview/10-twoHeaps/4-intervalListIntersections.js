665abec32951bb51b73b68e8ccf52542cb844ca1
adrian <adrianadames@gmail.com>
2022-04-30 20:22:23 -0400

    Trivial edits to problem statements to improve readability.

--- grokkingCodeInterview/10-twoHeaps/4-intervalListIntersections.js
+++ grokkingCodeInterview/10-twoHeaps/4-intervalListIntersections.js
@@ -16,12 +16,14 @@ closed interval.  For example, the intersection of [1, 3] and [2, 4] is
 
 Example 1:
 
-Input: A = [[0,2],[5,10],[13,23],[24,25]], B = [[1,5],[8,12],[15,24],[25,26]]
-Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+Input: 
+    A = [[0,2],[5,10],[13,23],[24,25]], 
+    B = [[1,5],[8,12],[15,24],[25,26]]
+Output: 
+    [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
 
 Note:
-
-0 <= A.length < 1000
-0 <= B.length < 1000
-0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
+    0 <= A.length < 1000
+    0 <= B.length < 1000
+    0 <= A[i].start, A[i].end, B[i].start, B[i].end < 10^9
 */
