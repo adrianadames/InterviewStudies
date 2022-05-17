a5b67da92f61208bf98c1becf686241ca3b90e8f
adrian <adrianadames@gmail.com>
2022-05-17 00:15:32 -0400

    Finished drafting solution for 2-slidingWindowMedian problem.

--- grokkingCodeInterview/10-twoHeaps/2-slidingWindowMedian.js
+++ grokkingCodeInterview/10-twoHeaps/2-slidingWindowMedian.js
@@ -36,100 +36,96 @@ input array's size for non-empty array.
 
 import MedianTracker from './1-medianOfNumberStream.js';
 
-// let slidingWindowMedian = (nums, k) => {
-//     let windowRunwayLength = nums.length - k; 
-//     let medians = [];
-//     let medianTracker = new MedianTracker();
-    
-//     // median of start position
-//     for (let i = 0; i< k; i++) {
-//         medianTracker.addNum(nums[i]);
-//     }
-//     // console.log('medianTracker: ', medianTracker);
-//     medians[0] = medianTracker.findMedian();
-
-//     for (let i = 0; i< windowRunwayLength; i++) {
-//         let firstNum = nums[i];
-//         let lastNum = nums[i+k-1];
-//         console.log('firstNum: ', firstNum);
-//         console.log('lastNum: ', lastNum)
-
-//         console.log('num to remove: ', firstNum);
-//         console.log('num to add: ', nums[i+k])
-//     }
-// }
-
-// naive way
 let slidingWindowMedian = (nums, k) => {
     let windowRunwayLength = nums.length - k; 
     let medians = [];
     let medianTracker = new MedianTracker();
     
-    // median of start position
+    // start position window median
     for (let i = 0; i< k; i++) {
         medianTracker.addNum(nums[i]);
     }
-    // console.log('medianTracker: ', medianTracker);
     medians[0] = medianTracker.findMedian();
 
+    // median for each window position
     for (let i = 1; i<= windowRunwayLength; i++) {
-        let numRemoved = nums[i-1];
-        let numAdded = nums[i+k-1];
-        console.log('numRemoved: ', numRemoved);
-        console.log('numAdded: ', numAdded);
+        // console.log('medians: ', medians);
+        let numToRemove = nums[i-1];
+        let numToAdd = nums[i+k-1];
+        // console.log('numToRemove: ', numToRemove);
+        // console.log('numToAdd: ', numToAdd);
 
-        // -we're removing a node and adding another; 
-        // -we need to remove numRemoved from whatever heap it's in;
+        // -if numToRemove less than or equal to maxHeap.maximum(), it's in the maxHeap
+        // (i.e., it's value part of the smaller half of all the values in the window)
+        if (numToRemove <= medianTracker.maxHeap.maximum()) {
+            for (let i = 0; i < medianTracker.maxHeap.items.length; i++) {
+                // -identify index of numToRemove in the maxHeap
+                if (medianTracker.maxHeap.items[i] === numToRemove) {
+                    // -swap value with last value in heap, 
+                    let maxHeapSize = medianTracker.maxHeap.items.length;
+                    let lastValueInHeap = medianTracker.maxHeap.items[maxHeapSize-1];
+                    medianTracker.maxHeap.items[i] = lastValueInHeap;    
+                    // change size of heap and median Tracker
+                    medianTracker.maxHeap.size -= 1; 
+                    medianTracker.size -=1;
+                    // bubble down the value that was swapped into numToRemove's position
+                    medianTracker.maxHeap.bubbleDown(i);
 
-        if (numRemoved <= medianTracker.maxHeap.maximum()) {
-            // then here i thought i would find whatever index numRemoved is at in the maxHeap and sift it down
-            // for (let i = 0; i < maxHeap.size; i++) {
-            //  if (numRemoved = maxHeap.items[i]) {
-            //      maxHeap.items[i] === -100
-            //      // then sift it down
-            //      // then swap it, then sift down whatever you swapped it with
-            //      // then reduce size of it and the medianTracker by 1
-            // }
+                    // -if window size even, maxHeap had same size as minHeap
+                    // -removing from maxHeap means we need to pop off minHeap min and 
+                    // add it to the maxHeap, making minHeap temporarily smaller; 
+                    // -then we add the new number, making the heaps the same size again
+                    if (k % 2 === 0) {
+                        medianTracker.maxHeap.add(medianTracker.minHeap.extractMinimum()); 
+                        medianTracker.addNum(numToAdd);
+                        medians.push(medianTracker.findMedian());
+                        break;
+                    } else {
+                        // -window size was odd, meaning maxHeap had an extra value
+                        medianTracker.addNum(numToAdd);
+                        medians.push(medianTracker.findMedian());
+                        break;
+                    }
+                }
+            }
+        } 
+        // -if numToRemove greater than or equal to minHeap.minimum(), it's in the minHeap
+        // (i.e., it's value part of the larger half of all the values in the window)
+        else if (numToRemove >= medianTracker.minHeap.minimum()) {
+            for (let i = 0; i < medianTracker.minHeap.items.length; i++) {
+                // -identify index of numToRemove in the minHeap
+                if (medianTracker.minHeap.items[i] === numToRemove) {
+                    // -swap value with last value in heap, 
+                    let minHeapSize = medianTracker.minHeap.items.length;
+                    let lastValueInHeap = medianTracker.minHeap.items[minHeapSize-1];
+                    medianTracker.minHeap.items[i] = lastValueInHeap;    
+                    // change size of heap and median Tracker
+                    medianTracker.minHeap.size -= 1; 
+                    medianTracker.size -=1;
+                    // bubble down the value that was swapped into numToRemove's position
+                    medianTracker.minHeap.bubbleDown(i);
 
+                    // -if window size odd, minHeap had one fewer than maxHeap, but
+                    // now it has two less; need to pop off from maxHeap and add to 
+                    // minHeap to get the sizes of the minHeap and maxHeap equal and their
+                    // sizes equal, and then we add the num making the medianTracker size odd again as desired
+                    if (k % 2 !== 0) {
+                        medianTracker.minHeap.add(medianTracker.maxHeap.extractMax()); 
+                        medianTracker.addNum(numToAdd);
+                        medians.push(medianTracker.findMedian());
+                        break;
+                    } else {
+                        // -window size was even, meaning minHeap had same size as maxHeap 
+                        medianTracker.addNum(numToAdd);
+                        medians.push(medianTracker.findMedian());
+                        break;
+                    }
+                }
+            }
         }
-
-        // -if the number we're removing is less than or equal to maxHeap.maximum(), it's in there. 
-        // -we can get rid of it by first making it negative infinity and sifting it down.
-        // -at this point, it will be leaf
-        // -then, we swap it with the last value in the maxHeap, making it so that the last value in
-        // the maxHeap arr (the last leaf) is negative infinity. after swapping, we make sure
-        // the the value swapped into the new position satisfies the heap rule by using bubble up function on it
-        // -then we decrease the size of the maxHeap by 1 and the medianTracker by 1
-        // -at this point, the maxHeap and meanHeap will have same number of values
-        // -then we add the numToAdd to medianTracker (whether it be the maxHeap or the minHeap) 
-        // and it stays odd
-
-        // -if the number we're removing is less than or equal to maxHeap.maximum() AND
-        // medianTracker.size is even, then we do as mentioned above. we change it to negative 
-        // infinity, bubble it down, swap it with the last leaf, and reduce the size of maxHeap by one
-        // -then we remove the min from minHeap and add it to maxHeap
-        // -then we add the numAdd so medianTracker.size stays even 
-
-        
-        // -now, if the number we're removing is greater than maxHeap.maximum(), 
-        // then we know it's in the minHeap. 
-        // -we get rid of it by first making it positive infinity and sifting it down
-        // -then we swap it with the last leaf and reduce the size of minHeap by one and the 
-        // size of the medianTracker by 1
-
-        // -if medianTracker.size was initially odd before the removal, then maxHeap had one 
-        // more node value. that means the right now, maxHeap has two more node values than minHeap
-        // -we pop off maxHeap.maximum() and put it in minHeap to get back balance (same number of nodes on each side)
-        // -then we add the numToAdd to medianTracker to get an odd number of values back 
-
-        // -if medianTracker.size was initially even before the removal, 
-        // after numToRemove is removed from minHeap, minHeap will have one less value than maxHeap, 
-        // which means that we're still balanced (albeit with an odd number of values)
-        // -we add numToAdd via MedianTracker to bring our total number of values back to the 
-        // even value we originally started with
-
     }
+    return medians
 }
 
 let nums1 = [1, 3, -1, -3, 5, 3, 6, 7];
-slidingWindowMedian(nums1, 3);
+console.log('slidingWindowMedian', slidingWindowMedian(nums1, 3));
