c55348d328f4d9625e153ce3cd4ac171200b0955
adrian <adrianadames@gmail.com>
2022-05-28 15:32:30 -0400

    Got recursive, recursive + memoized (top-down), and tabulated (bottom-up) solutions, but they're not complete as they currently don't return the list of items associated with the maximum profit for a given capacity.

--- grokkingCodeInterview/16-knapsackDynamicProgramming/1-zeroOneKnapsack.js
+++ grokkingCodeInterview/16-knapsackDynamicProgramming/1-zeroOneKnapsack.js
@@ -31,226 +31,133 @@ item can only be selected once, which means either we put an item in the
 knapsack or we skip it.
 */
 
-// -most concise and straightforward (2^n) recursive approach
-// -doesn't return set of weights associated with highest profit; only the largest profit itself
-let knapsack0 = (weights, profits, capacity, index = 0) => {
+// -recursive approach (time O(n) = 2^n) 
+// -note: doesn't return set of weights associated with highest profit; only the largest profit itself
+let knapsackRec = (weights, profits, capacity, index = 0) => {
     // base case
-    if (index === profits.length || capacity === 0) {
-        return 0;
+    // -if number of items remaining is zero (i.e. index = num of items), no more profits
+    // -if remaining capacity is zero, no more profits
+    if (index === weights.length || capacity === 0) {
+        return 0
     }
-
-    // if weight weights[index] greater than capacity, don't include in optimal sol'n
+    // if item weights[index] greater than capacity, skip it (i.e. don't include in optimal sol'n)
     if (weights[index] > capacity) {
-        return knapsack0(weights, profits, capacity, index+1)
+        return knapsackRec(weights, profits, capacity, index+1)
     } else {
-        // return maximum between the subsets associated with the included item
-        // and the subsets without the included item
-        return Math.max(
-            knapsack0(weights, profits, capacity, index+1),  // without weights[index]
-            profits[index] + knapsack0(weights, profits, capacity-weights[index], index + 1) // with weights[index]
-        )
+        // return larger b/t profit for subsets including item and 
+        // profit for subsets excluding item
+        let profitExcludingItem = knapsackRec(weights, profits, capacity, index + 1);
+        let profitIncludingItem = profits[index] + knapsackRec(weights, profits, capacity-weights[index], index + 1);
+        let maxProfit = Math.max(profitExcludingItem, profitIncludingItem);
+        return maxProfit
     }
 }
 
-//-more wordy but straightforward (2^n) recursive approach
-let knapsack1 = (weights, profits, capacity, index = 0, maxProfit = [0]) => {
+// top-down memoized solution
+// -store computed values 
+// -two changing parameters: index and capacity => store will be 2d mtx
+// -store[index][capacity] will contain answer for function call 
+// knapsack(weights, profits, capacity, index)
+
+// -since memoization arr stores results for all subprobs, we can conclude
+// that we won't have more than numItems * Capacity subprobs => time O(n) = numItems * Capacity
+// -O(numItems * capacity) space for memoization arr and O(numItems) for recursive stack 
+// => space O(n) =  O(numItems * capacity + numItems) = O(numItems * capacity)
+let knapsackRecMemoizedWrapper = (weights, profits, capacity) => {
+    let store = new Array(weights.length + 1).fill(null).map(arrItem => new Array(capacity+1).fill(-1));
+    return knapsackRecMemoized(weights, profits, capacity, 0, store)
+}
+
+let knapsackRecMemoized = (weights, profits, capacity, index = 0, store) => {
     // base case
-    if (index === profits.length || capacity === 0) {
-        return 0;
+    // -if number of items remaining is zero (i.e. index = num of items), no more profits
+    // -if remaining capacity is zero, no more profits
+    if (index === weights.length || capacity === 0) {
+        return 0
     }
 
-    // if weight weights[index] greater than capacity, don't include in optimal sol'n
-    if (weights[index] > capacity) {
-        return knapsack1(weights, profits, capacity, index+1, maxProfit)
-    } else {
-        let withoutItemProfit = -1;
-        let withItemProfit = -1;
+    // check if answer already present in store
+    if (store[index][capacity] !== -1) {
+        // console.log('index: ', index, ' capacity: ', capacity);
+        // console.log('stored: ', store[index][capacity]);
+        return store[index][capacity]
+    }
 
-        for (let k = 0; k <= 1; k++) {
-            // item at index (i.e. weights[index]) not included 
-            if (k === 0) {
-                withoutItemProfit = knapsack1(weights, profits, capacity, index+1, maxProfit);
-            }
-            // item at index included in the knapsack
-            if (k === 1) {
-                withItemProfit = profits[index] + knapsack1(weights, profits, capacity-weights[index], index + 1, maxProfit);
-                // console.log('withItemProfit: ', withItemProfit);
-                // subset.pop(index);
+    // if item weights[index] greater than capacity, skip it (i.e. don't include in optimal sol'n)
+    if (weights[index] > capacity) {
+        store[index][capacity] = knapsackRecMemoized(weights, profits, capacity, index+1, store);
+        return store[index][capacity]
+    } else {
+        // return larger b/t profit for subsets including item and 
+        // profit for subsets excluding item
+        let profitExcludingItem = knapsackRecMemoized(weights, profits, capacity, index+1, store);
+        let profitIncludingItem = profits[index] + knapsackRecMemoized(weights, profits, capacity-weights[index], index+1, store);
+        let maxProfit = Math.max(profitExcludingItem, profitIncludingItem);
+        store[index][capacity] = maxProfit;
+        return store[index][capacity]
+    }
+}
+
+// bottom-up tabulated approach
+// -time and space O(n) = O(numItems * capacity)
+// -see grokking solution and https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/ for similar algorithm
+// with space O(n) = 2* numItems
+// -see https://www.es.ele.tue.nl/education/5MC10/Solutions/knapsack.pdf and
+// http://cse.unl.edu/~goddard/Courses/CSCE310J/Lectures/Lecture8-DynamicProgramming.pdf 
+// for solution showing how to get list of items corresponding to the optimal solution
+let knapsackTabulated = (weights, profits, capacity) => {
+    // create store matrix dimensions i (rows) by j (columns)
+    // store[i][j] = max profit given up to i items and j capacity
+    // store[0][j] (zero items) and store[i][0] (zero capacity) will always be zero
+    let store = new Array(weights.length + 1).fill(null).map(arrItem => new Array(capacity+1).fill(0));
+
+    for (let i = 1; i <= weights.length; i++) {
+        for (let j = 1; j <= capacity; j++) {
+            if (weights[i-1] > j) {
+                store[i][j] = store[i-1][j];
+            } else {
+                let profitExcludingItem = store[i-1][j]; // same as max profit given i-1 items
+                let profitIncludingItem = profits[i-1] + store[i-1][j-weights[i-1]];
+                let maxProfit = Math.max(profitExcludingItem, profitIncludingItem);
+                store[i][j] = maxProfit;
             }
         }
-
-        let currentMaxProfit = Math.max(withoutItemProfit, withItemProfit);
-        if (currentMaxProfit > maxProfit[0]) {
-            maxProfit[0] = currentMaxProfit;
-            // console.log('new maxProfit[0]: ', maxProfit[0]);
-        } 
-
-        return currentMaxProfit
     }
+    return store[weights.length][capacity]
 }
-
-// -approach where I tried (and failed) to get the subset associated with the highest profit;
-// using the 2^n recursive approach from above
-// -don't understand how to deal with the subset.pop() for going back up the recursion tree
-let maxProfitKnapsackWrapper1 = (weights, profits, capacity) => {
-    let maxProfit = [0];
-    let subset = [];
-    let maxSubset = [];
-    let index = 0;
-    let allSubsets = [];
-    maxProfitKnapsack1(weights, profits, capacity, index, subset, maxSubset, maxProfit, allSubsets); 
-    return [maxProfit, maxSubset]
-}
-let maxProfitKnapsack1 = (weights, profits, capacity, index, subset, maxSubset, maxProfit, allSubsets) => {
-    // base case
-    if (index === profits.length || capacity === 0) {
-        allSubsets.push(subset.slice());
-        // console.log('allSubsets: ', allSubsets);
-        return 0;
-    }
-
-    // if weight weights[index] greater than capacity, don't include in optimal sol'n
-    if (weights[index] > capacity) {
-        // console.log('adding weight ', weights[index] ,' will exceed capacity');
-        return maxProfitKnapsack1(weights, profits, capacity, index+1, subset, maxSubset, maxProfit, allSubsets)
-    } else {
-        let withoutItemProfit = -1;
-        let withItemProfit = -1;
-
-        for (let k = 0; k <= 1; k++) {
-            // item at index (i.e. weights[index]) not included 
-            if (k === 0) {
-                withoutItemProfit = maxProfitKnapsack1(weights, profits, capacity, index+1, subset, maxSubset, maxProfit, allSubsets);
-            }
-            // item at index included in the knapsack
-            if (k === 1) {
-                console.log('withItemProfit1: ', withItemProfit);
-                console.log('subset1a: ', subset);
-                subset.push(index);
-                console.log('subset1b: ', subset);
-                withItemProfit = profits[index] + maxProfitKnapsack1(weights, profits, capacity-weights[index], index + 1, subset, maxSubset, maxProfit, allSubsets);
-                
-                console.log('withItemProfit2: ', withItemProfit);
-                console.log('subset2: ', subset);
-                // subset.pop();
-            }
-        }
-
-        let currentMaxProfit = Math.max(withoutItemProfit, withItemProfit);
-        
-        if (currentMaxProfit > maxProfit[0]) {
-            maxProfit[0] = currentMaxProfit;
-            // console.log('new maxProfit[0]: ', maxProfit[0]);
-            maxSubset[0] = subset.slice();
-            // console.log('new maxSubset: ', maxSubset);
-            subset.pop();
-        } 
-
-        return currentMaxProfit
-    }
-}
-
-
-// -similar approach where I tried (and failed even more badly) to get the subset associated with the highest profit;
-// using the 2^n recursive approach from above
-// -don't understand how to deal with the subset.pop() for going back up the recursion tree
-let maxProfitKnapsackWrapper2 = (weights, profits, capacity) => {
-    let maxProfit = [0];
-    let subset = [];
-    let maxSubset = [];
-    let index = 0;
-    let allSubsets = [];
-    maxProfitKnapsack2(weights, profits, capacity, index, subset, maxSubset, maxProfit, allSubsets); 
-    return [maxProfit, maxSubset]
-}
-let maxProfitKnapsack2 = (weights, profits, capacity, index, subset, maxSubset, maxProfit, allSubsets) => {
-    // base case
-    if (index === profits.length || capacity === 0) {
-        allSubsets.push(subset.slice());
-        console.log('subset base case: ', subset);
-        console.log('base case maxProfit: ', maxProfit);
-        return 0;
-    }
-
-    // if weight weights[index] greater than capacity, don't include in optimal sol'n
-    if (weights[index] > capacity) {
-        // console.log('adding weight ', weights[index] ,' will exceed capacity');
-        return maxProfitKnapsack2(weights, profits, capacity, index+1, subset, maxSubset, maxProfit, allSubsets)
-    } else {
-        let withoutItemProfit = -1;
-        let withItemProfit = -1;
-
-        for (let k = 0; k <= 1; k++) {
-            // item at index (i.e. weights[index]) not included 
-            if (k === 0) {
-                withoutItemProfit = maxProfitKnapsack2(weights, profits, capacity, index+1, subset, maxSubset, maxProfit, allSubsets);
-                console.log('withoutItemProfit: ', withoutItemProfit);
-
-                if (withoutItemProfit > maxProfit[0]) {
-                    maxProfit[0] = withoutItemProfit;
-                    console.log('subset without: ', subset);
-                    console.log('maxProfit[0]: ', maxProfit[0]);
-                }
-            }
-            // item at index included in the knapsack
-            if (k === 1) {
-                console.log('withItemProfit1: ', withItemProfit);
-                console.log('subset1a: ', subset);
-                subset.push(index);
-                console.log('subset1b: ', subset);
-                withItemProfit = profits[index] + maxProfitKnapsack2(weights, profits, capacity-weights[index], index + 1, subset, maxSubset, maxProfit, allSubsets);
-                
-                console.log('withItemProfit2: ', withItemProfit);
-                if (withItemProfit > maxProfit[0]) {
-                    maxProfit[0] = withItemProfit;
-                    maxSubset[0] = subset.slice();
-                    subset.pop();
-                } else {
-                    continue
-                }
-                console.log('maxProfit[0]: ', maxProfit[0]);
-                console.log('subset2: ', subset);
-                console.log('maxSubset: ', maxSubset);
-                subset.pop();
-            }
-        }
-
-        let currentMaxProfit = Math.max(withoutItemProfit, withItemProfit);
-        
-        if (currentMaxProfit > maxProfit[0]) {
-            maxProfit[0] = currentMaxProfit;
-            // console.log('new maxProfit[0]: ', maxProfit[0]);
-            maxSubset[0] = subset.slice();
-            // console.log('new maxSubset: ', maxSubset);
-            subset.pop();
-        } 
-
-        return currentMaxProfit
-    }
-}
-
 
 // max profit = 10
-let weights1 = [2, 3, 1, 4]; 
-let profits1 = [4, 5, 3, 7];
-let capacity1 = 5;
+let weights1a = [2, 3, 1, 4]; 
+let profits1a = [4, 5, 3, 7];
+let capacity1a = 5;
+
+// max profit = 10 (same as above, but items in different order)
+let weights1b = [1, 4, 3, 2]; 
+let profits1b = [3, 7, 5, 4];
+let capacity1b = 5;
 
 // max profit = 220
 let weights2 = [10, 20, 30]; 
 let profits2 = [60, 100, 120];
 let capacity2 = 50;
 
-let weights3 = [1, 4, 3, 2]; 
-let profits3 = [3, 7, 5, 4];
-let capacity3 = 5;
+// max profit = 17
+let weights3 = [3, 6, 9, 5]; 
+let profits3 = [7, 2, 10, 4];
+let capacity3 = 15;
 
+console.log('knapsackRec: ', knapsackRec(weights1a, profits1a, capacity1a));
+console.log('knapsackRec: ', knapsackRec(weights1b, profits1b, capacity1b));
+console.log('knapsackRec: ', knapsackRec(weights2, profits2, capacity2));
+console.log('knapsackRec: ', knapsackRec(weights3, profits3, capacity3));
 
-// console.log('knapsack0: ', knapsack0(weights1, profits1, capacity1));
+console.log('knapsackRecMemoizedWrapper: ', knapsackRecMemoizedWrapper(weights1a, profits1a, capacity1a));
+console.log('knapsackRecMemoizedWrapper: ', knapsackRecMemoizedWrapper(weights1b, profits1b, capacity1b));
+console.log('knapsackRecMemoizedWrapper: ', knapsackRecMemoizedWrapper(weights2, profits2, capacity2));
+console.log('knapsackRecMemoizedWrapper: ', knapsackRecMemoizedWrapper(weights3, profits3, capacity3));
 
-// console.log('knapsack1: ', knapsack1(weights1, profits1, capacity1));
-// console.log('maxProfitKnapsackWrapper1: ', maxProfitKnapsackWrapper1(weights2, profits2, capacity2));
-// console.log('maxProfitKnapsackWrapper1: ', maxProfitKnapsackWrapper1(weights1, profits1, capacity1));
-// console.log('maxProfitKnapsackWrapper1: ', maxProfitKnapsackWrapper1(weights3, profits3, capacity3));
+console.log('knapsackTabulated: ', knapsackTabulated(weights1a, profits1a, capacity1a));
+console.log('knapsackTabulated: ', knapsackTabulated(weights1b, profits1b, capacity1b));
+console.log('knapsackTabulated: ', knapsackTabulated(weights2, profits2, capacity2));
+console.log('knapsackTabulated: ', knapsackTabulated(weights3, profits3, capacity3));
 
-console.log('maxProfitKnapsackWrapper2: ', maxProfitKnapsackWrapper2(weights3, profits3, capacity3));
-console.log('maxProfitKnapsackWrapper2: ', maxProfitKnapsackWrapper2(weights2, profits2, capacity2));
