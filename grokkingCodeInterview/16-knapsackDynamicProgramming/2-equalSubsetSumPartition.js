907000a3a5ef4fc35260606a8ab6ada285771a2c
adrian <adrianadames@gmail.com>
2022-06-27 01:35:02 -0400

    Made decent progress on four out of six problems. Still need to code the bottom-up dynamic programming approach for all the problems. Still need to do run- and space-time analysis for all the problems.

--- grokkingCodeInterview/16-knapsackDynamicProgramming/2-equalSubsetSumPartition.js
+++ grokkingCodeInterview/16-knapsackDynamicProgramming/2-equalSubsetSumPartition.js
@@ -24,3 +24,144 @@ Example 3:
     Explanation: The given set cannot be partitioned into two subsets with 
     equal sum.
 */
+
+let equalSubsetSumPartitionRec = (seq) => {
+    let targetSum = seq.reduce((prev, curr) => prev + curr);
+    if (targetSum % 2 !== 0) {
+        return false
+    } 
+    targetSum = targetSum/2;
+    let targetSubsets = [];
+    let subset = [];
+    let subsetSum = 0;
+    let index = 0;
+
+    let findEqualSubsetSumPartition = (seq, index, subset, subsetSum, targetSum, targetSubsets) => {
+        // base cases
+        if (subsetSum === targetSum) { // if subset sum equals target sum, equal partition possible exists
+            return targetSubsets.push(subset.slice())
+        }
+        if (index === seq.length) {
+            return 
+        }
+        
+        // -if current subset sum plus current element greater than targetSubset,
+        // any sets downstream in the recursion tree not eligible as a targetSubset
+        if (subsetSum + seq[index] > targetSum) {
+            // not including element at index
+            findEqualSubsetSumPartition(seq, index +1, subset, subsetSum, targetSum, targetSubsets);
+        } else {
+            // not including element at index
+            findEqualSubsetSumPartition(seq, index +1, subset, subsetSum, targetSum, targetSubsets);
+
+            // including element at index
+            subset.push(seq[index]);
+            findEqualSubsetSumPartition(seq, index +1, subset, subsetSum + seq[index], targetSum, targetSubsets);
+            subset.pop();
+        }     
+    }
+    findEqualSubsetSumPartition(seq, index, subset, subsetSum, targetSum, targetSubsets);
+    return [targetSubsets.length > 0, targetSubsets]
+}
+
+// console.log('equalSubsetSumPartitionRec: ', equalSubsetSumPartitionRec([1,2,3]))
+
+let equalSubsetSumPartitionMemoized = (seq) => {
+    let targetSum = seq.reduce((prev, curr) => prev + curr);
+    if (targetSum % 2 !== 0) {
+        return false
+    }
+    targetSum = targetSum/2;
+    let targetSubsets = [];
+    let subset = [];
+    let subsetSum = 0;
+    let allSubsets = [];
+    let index = 0;
+    let store = new Array(seq.length + 1).fill(null).map(() => new Array(targetSum+1).fill(-1));
+
+    let findEqualSubsetSumPartition = (seq, index, subset, subsetSum, targetSum, targetSubsets, store, allSubsets) => {
+        // base cases
+        if (subsetSum === targetSum) {
+            targetSubsets.push(subset.slice());
+            if (index === seq.length) {
+                allSubsets.push(subset.slice());
+            }
+            return true
+        } else if (index === seq.length) {
+            allSubsets.push(subset.slice());
+            return false
+        }
+
+        // check if problem previously solved
+        if (store[index][subsetSum] !== -1) {
+            return store[index][subsetSum]
+        }
+        // -check if including element at index will put us over the target sum
+        // -if so, including the element at index given the subsetSum will not produce the targetSum
+        // -move on to checking subsets that don't include that element
+        if (subsetSum + seq[index] > targetSum) {
+            store[index][subsetSum] = false;
+            return findEqualSubsetSumPartition(seq, index+1, subset, subsetSum, targetSum, targetSubsets, store, allSubsets);
+        } else {
+            // not including element at index
+            let withoutElement = findEqualSubsetSumPartition(seq, index + 1, subset, subsetSum, targetSum, targetSubsets, store, allSubsets);            
+
+            // including element at index
+            subset.push(seq[index]);
+            let withElement = findEqualSubsetSumPartition(seq, index + 1, subset, subsetSum+seq[index], targetSum, targetSubsets, store, allSubsets);
+            subset.pop();
+            
+            // -stores whether the target sum can be reached given the starting subsetSum below 
+            // and any subsets that can be made with any of the elements at or to the right of the index below
+            return store[index][subsetSum] = withoutElement || withElement;
+        }
+    }
+    findEqualSubsetSumPartition(seq, index, subset, subsetSum, targetSum, targetSubsets, store, allSubsets);
+    console.log('store: ', store);
+    console.log('targetSubsets: ', targetSubsets);
+    return store[0][0]
+}
+
+// console.log('equalSubsetSumPartitionMemoized: ', equalSubsetSumPartitionMemoized([1,2,3]));
+// console.log('equalSubsetSumPartitionMemoized: ', equalSubsetSumPartitionMemoized([3,1,2]));
+// console.log('equalSubsetSumPartitionMemoized: ', equalSubsetSumPartitionMemoized([3,2,1]));
+console.log('equalSubsetSumPartitionMemoized: ', equalSubsetSumPartitionMemoized([5, 7, 2, 10]));
+// console.log('equalSubsetSumPartitionMemoized: ', equalSubsetSumPartitionMemoized([1,2,3,4,5,1]));
+
+// // same function as above, but deleted all comments for easy viewing on python tutor
+// let equalSubsetSumPartitionMemoizedForPythonTutor = (seq) => {
+//     let targetSum = seq.reduce((prev, curr) => prev + curr);
+//     if (targetSum % 2 !== 0) {
+//         return false
+//     }
+//     targetSum = targetSum/2;
+//     let store = new Array(seq.length + 1).fill(null).map(() => new Array(targetSum+1).fill(-1));
+//     let findEqualSubsetSumPartition = (seq, targetSum,store, index = 0, subset = [], subsetSum =0,  targetSubsets =[],  allSubsets=[]) => {
+//         if (subsetSum === targetSum) {
+//             targetSubsets.push(subset.slice());
+//             if (index === seq.length) {
+//                 allSubsets.push(subset.slice());
+//             }
+//             return true
+//         } else if (index === seq.length) {
+//             allSubsets.push(subset.slice());
+//             return false
+//         }
+//         if (store[index][subsetSum] !== -1) {
+//             return store[index][subsetSum]
+//         }
+//         if (subsetSum + seq[index] > targetSum) {
+//             store[index][subsetSum] = false;
+//             return findEqualSubsetSumPartition(seq, targetSum,store, index+1, subset, subsetSum,  targetSubsets,  allSubsets);
+//         } else {
+//             let withoutElement = findEqualSubsetSumPartition(seq, targetSum,store, index+1, subset, subsetSum,  targetSubsets,  allSubsets);            
+//             subset.push(seq[index]);
+//             let withElement = findEqualSubsetSumPartition(seq, targetSum,store, index+1, subset, subsetSum + seq[index],  targetSubsets,  allSubsets);
+//             subset.pop();  
+//             return store[index][subsetSum] = withoutElement || withElement;
+//         }
+//     }
+//     findEqualSubsetSumPartition(seq, targetSum,store);
+//     return store[0][0]
+// }
+// console.log('equalSubsetSumPartitionMemoizedForPythonTutor: ', equalSubsetSumPartitionMemoizedForPythonTutor([1,2,3,4,5,1]));
--- grokkingCodeInterview/16-knapsackDynamicProgramming/4-minimumSubsetSumDifference.js
+++ grokkingCodeInterview/16-knapsackDynamicProgramming/4-minimumSubsetSumDifference.js
@@ -27,3 +27,98 @@ Example 3:
     absolute difference between the sum of numbers is '92'. Here are the two 
     subsets: {1, 3, 4} & {100}.
 */
+
+// -so the way i'm thinking about this, i'd have to what? 
+// -for every subset, i'd need to calculate the difference between that subset sum
+// and the sum of every element that isn't in that subset
+// -if a subset's sum is X, then the sum of the other subset is totalSubset-X
+
+let minimumSubsetSumDifferenceRec = (seq) => {
+    let totalSubsetSum = seq.reduce((prev, curr) => prev + curr);
+    let index = 0;
+    let targetSubset = [];
+    let subset = [];
+    let subsetSum = [0];
+    let minDifference = [Infinity];
+
+    let findMinimumSubsetSumDifference = (seq, index, subset, subsetSum, targetSubset, totalSubsetSum, minDifference) => {
+        // base case
+        if (index === seq.length) {
+            return 
+        }
+
+        if (subset.length > 0) {
+            let complementSubsetSum = totalSubsetSum-subsetSum; 
+    
+            if (Math.abs(complementSubsetSum-subsetSum[0]) < minDifference[0]) {
+                minDifference[0] = Math.abs(complementSubsetSum-subsetSum[0]);
+                targetSubset.pop();
+                targetSubset.push(subset.slice());
+            }
+        }
+        
+        // not including element at index
+        findMinimumSubsetSumDifference(seq, index+1, subset, subsetSum, targetSubset, totalSubsetSum, minDifference);            
+        
+        // including element at index
+        subset.push(seq[index]);
+        subsetSum[0] += seq[index];
+        findMinimumSubsetSumDifference(seq, index+1, subset, subsetSum, targetSubset, totalSubsetSum, minDifference);
+        subset.pop();
+        subsetSum[0] -= seq[index];     
+    }
+    findMinimumSubsetSumDifference(seq, index+1, subset, subsetSum, targetSubset, totalSubsetSum, minDifference);
+    return targetSubset
+}
+
+console.log('minimumSubsetSumDifferenceRec: ', minimumSubsetSumDifferenceRec([1, 3, 100, 4]))
+
+console.log('minimumSubsetSumDifferenceRec: ', minimumSubsetSumDifferenceRec([1, 2, 7, 1, 5]))
+
+
+// let minimumSubsetSumDifferenceMemoized = (seq) => {
+//     let totalSubsetSum = seq.reduce((prev, curr) => prev + curr);
+    
+//     // we are looking for the subset who's sum is closest to totalSubsetSum/2; 
+//     targetSum = totalSubsetSum/2;
+//     let index = 0;
+//     let targetSubset = [];
+//     let subset = [];
+//     let store = new Array(seq.length + 1).fill(null).map(() => new Array(targetSum+1).fill(-1));
+
+//     // -as we go through each subset, we pass along the difference between the subsetSum
+//     // and the targetSum
+//     // -if the absolute value of that difference is smaller than that passed down, that
+//     // subset becomes the new targetSubset
+//     let findMinimumSubsetSumDifference = (seq, index, targetSum, targetSubset, subset, store) => {
+//         // base cases
+//         if (targetSum === 0) {
+//             return true
+//         }
+//         if (index === seq.length) {
+//             return false
+//         }
+
+//         // if problem previously solved
+//         if (store[index][targetSum] !== -1) {
+//             return store[index][targetSum]
+//         }
+        
+//         // if targetSum less than next element, any sets downstream recursion tree
+//         // not eligible as a targetSubset (i.e. don't search downstream sets w/ element)
+//         if (targetSum < seq[index]) {
+//             store[index][targetSum] = false;
+//             return findMinimumSubsetSumDifference(seq, index + 1, targetSum, store);
+//         } else {
+//             // not including element at index
+//             let withoutElement = findMinimumSubsetSumDifference(seq, index + 1, targetSum, store);            
+//             // including element at index
+//             let withElement = findMinimumSubsetSumDifference(seq, index + 1, targetSum - seq[index], store);
+
+//             return store[index][targetSum] = withoutElement || withElement
+//         }     
+//     }
+//     return findMinimumSubsetSumDifference(seq, index, targetSum, store);
+// }
+
+// console.log('minimumSubsetSumDifferenceMemoized: ', minimumSubsetSumDifferenceMemoized([1,2,3]))
