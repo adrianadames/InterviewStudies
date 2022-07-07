bd04a706c5c625c5bd165eb3c3d7b840034f921a
adrian <adrianadames@gmail.com>
2022-07-06 20:15:27 -0400

    Finished recursive solutions for last two problems. Still need to work on memoized and tabulated solutions.

--- grokkingCodeInterview/16-knapsackDynamicProgramming/5-countOfSubsetSum.js
+++ grokkingCodeInterview/16-knapsackDynamicProgramming/5-countOfSubsetSum.js
@@ -17,3 +17,38 @@ Example 2:
     Output: 3
     The given set has '3' subsets whose sum is '9': {2, 7}, {1, 7, 1}, {1, 2, 1, 5}
 */
+
+// -given set of positive numbers, find the total number of subsets who sum 
+// is equal to a given number 'S'
+
+let countOfSubsetSum = (seq, target) => {
+    let subset = []; 
+    let subsetSum = 0;
+    let allSubsets = [];
+    let targetSubsets = [];
+    let index = 0;  
+
+    let findSubsetsSummingToTarget = (seq, target, subset = [], subsetSum= 0, allSubsets = [], targetSubsets = [], index = []) => {
+        // base cases
+        if (subsetSum === target) {
+            if (index === seq.length) {
+                allSubsets.push(subset.slice());
+            }
+            return targetSubsets.push(subset.slice());
+            // push to targetSubsets
+        }
+        if (index === seq.length) {
+            return allSubsets.push(subset.slice());
+        }
+
+        let withoutElement = findSubsetsSummingToTarget(seq, target, subset, subsetSum, allSubsets, targetSubsets, index + 1);
+        
+        subset.push(seq[index]);
+        let withElement = findSubsetsSummingToTarget(seq, target, subset, subsetSum += seq[index], allSubsets, targetSubsets, index+1);
+        subset.pop();
+    }
+    findSubsetsSummingToTarget(seq, target, subset, subsetSum, allSubsets, targetSubsets, index);
+    return targetSubsets
+}
+
+console.log('countOfSubsetSum: ', countOfSubsetSum([1, 2, 7, 1, 5], 9))
--- grokkingCodeInterview/16-knapsackDynamicProgramming/6-targetSum.js
+++ grokkingCodeInterview/16-knapsackDynamicProgramming/6-targetSum.js
@@ -20,3 +20,74 @@ Example 2:
     Explanation: The given set has '2' ways to make a sum of '9': 
     {+1+2+7-1} & {-1+2+7+1}
 */
+
+let targetSumRec = (seq, target) => {
+    let allSubsets = [];
+    let subset = [];
+    let subsetSum = 0;
+    let targetSubsets = [];
+    let index = 0;
+
+    let findTargetSubsets = (seq, target, allSubsets, subset, subsetSum, targetSubsets, index) => {
+        // base cases
+        if (index === seq.length) {
+            allSubsets.push(subset.slice())
+            if (subsetSum === target) {
+                targetSubsets.push(subset.slice());
+            }
+            return 
+        }
+
+        // -each number can be assigned a positive or negative sign
+        subset.push(seq[index]);
+        findTargetSubsets(seq, target, allSubsets, subset, subsetSum + seq[index], targetSubsets, index+1);
+        subset.pop();
+
+        subset.push(-1*seq[index]);
+        findTargetSubsets(seq, target, allSubsets, subset, subsetSum- seq[index], targetSubsets,index + 1);
+        subset.pop();
+    }
+
+    findTargetSubsets(seq, target, allSubsets, subset, subsetSum, targetSubsets, index);
+    console.log('allSubsets: ', allSubsets)
+    return targetSubsets
+}
+
+console.log('targetSumRec: ', targetSumRec([1, 1, 2, 3], 1))
+console.log('targetSumRec: ', targetSumRec([1, 2, 7, 1], 9))
+
+
+let targetSumMemoized = (seq, target) => {
+    let allSubsets = [];
+    let subset = [];
+    let subsetSum = 0;
+    let targetSubsets = [];
+    let index = 0;
+
+    let findTargetSubsets = (seq, target, allSubsets, subset, subsetSum, targetSubsets, index) => {
+        // base cases
+        if (index === seq.length) {
+            allSubsets.push(subset.slice())
+            if (subsetSum === target) {
+                targetSubsets.push(subset.slice());
+            }
+            return 
+        }
+
+        // -each number can be assigned a positive or negative sign
+        subset.push(seq[index]);
+        findTargetSubsets(seq, target, allSubsets, subset, subsetSum + seq[index], targetSubsets, index+1);
+        subset.pop();
+
+        subset.push(-1*seq[index]);
+        findTargetSubsets(seq, target, allSubsets, subset, subsetSum- seq[index], targetSubsets,index + 1);
+        subset.pop();
+    }
+
+    findTargetSubsets(seq, target, allSubsets, subset, subsetSum, targetSubsets, index);
+    console.log('allSubsets: ', allSubsets)
+    return targetSubsets
+}
+
+console.log('targetSumMemoized: ', targetSumMemoized([1, 1, 2, 3], 1))
+console.log('targetSumMemoized: ', targetSumMemoized([1, 2, 7, 1], 9))
