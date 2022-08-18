a342aba074d7f2ccf02816ec3fc7b715542fa00f
adrian <adrianadames@gmail.com>
2022-08-18 18:35:51 -0400

    Trivial formatting changes/edits to make problem statements easier to read.

--- grokkingCodeInterview/4-fastAndSlowPointers/5-palindromeLinkedList.js
+++ grokkingCodeInterview/4-fastAndSlowPointers/5-palindromeLinkedList.js
@@ -1,22 +1,21 @@
 /*
 (medium)
 
-Palindrome LinkedList (medium) #
-Given the head of a Singly LinkedList, write a method to check 
+Problem Statement: 
+-Given the head of a Singly LinkedList, write a method to check 
 if the LinkedList is a palindrome or not.
-
-Your algorithm should use constant space and the input 
+-Your algorithm should use constant space and the input 
 LinkedList should be in the original form once the algorithm is 
 finished. The algorithm should have O(N)O(N) time complexity 
 where ‘N’ is the number of nodes in the LinkedList.
 
 Example 1:
-
-Input: 2 -> 4 -> 6 -> 4 -> 2 -> null
-Output: true
-
+    Input: 2 -> 4 -> 6 -> 4 -> 2 -> null
+    Output: true
 Example 2:
-
-Input: 2 -> 4 -> 6 -> 4 -> 2 -> 2 -> null
-Output: false
+    Input: 2 -> 4 -> 6 -> 4 -> 2 -> 2 -> null
+    Output: false
 */
+
+// -palindrome: a word, phrase, or sequence that reads the 
+// same backward as forward, e.g., madam or nurses run.
