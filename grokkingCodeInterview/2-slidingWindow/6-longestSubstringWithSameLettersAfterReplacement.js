238ec3ddd31df12dda2d3605c22f4ef3ea450a55
adrian <adrianadames@gmail.com>
2024-06-29 01:55:44 -0400

    Done with problems 6 and 7.

--- grokkingCodeInterview/2-slidingWindow/6-longestSubstringWithSameLettersAfterReplacement.js
+++ grokkingCodeInterview/2-slidingWindow/6-longestSubstringWithSameLettersAfterReplacement.js
@@ -21,9 +21,38 @@ Example 3:
     Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
 */
 
-let longestSubstringWithSameLettersAfterReplacement = (str, k) => {
-}
 
-// console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('aabccbb', 2)); 
-// console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abbcb', 1));
-// console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abccde',1));
+let longestSubstringWithSameLettersAfterReplacement = (str, k) => {
+    let windowStart = 0;
+    let windowEnd = 0; 
+    let maxSubstringLength = 0; 
+    let substringLength = 0;
+    let distinctCharsTracker = {}
+    let maxFreq = 0;
+
+    while (windowEnd < str.length) {
+        if (!distinctCharsTracker[str[windowEnd]]) {
+            distinctCharsTracker[str[windowEnd]] = 0;
+        };
+        distinctCharsTracker[str[windowEnd]]++;
+
+        maxFreq = Math.max(maxFreq, distinctCharsTracker[str[windowEnd]]); 
+
+        if ((windowEnd - windowStart + 1 - maxFreq) > k) {
+            distinctCharsTracker[str[windowStart]]--;
+            windowStart++;
+        };
+
+        maxSubstringLength = Math.max(maxSubstringLength, windowEnd - windowStart + 1);
+
+        windowEnd++;
+    };
+
+    return maxSubstringLength
+};
+
+console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('aabccbb', 2)); 
+console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abbcb', 1));
+console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abccde',1));
+
+
