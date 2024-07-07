727ed0b87a05e17257e2759d7ce9c1d49312d926
adrian <adrianadames@gmail.com>
2024-07-07 19:07:31 -0400

    Done with problem 2.

--- grokkingCodeInterview/2-slidingWindow/2-smallestSubarrayWithGivenSum.js
+++ grokkingCodeInterview/2-slidingWindow/2-smallestSubarrayWithGivenSum.js
@@ -27,13 +27,36 @@ Output: 3
 Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] or [1, 1, 6].
 */
 
-function smallestSubarrayWithGivenSum(arr,S) {
-    
+// time complexity: O(n)
+// space complexity: O(1)
+function smallestSubarrayWithGivenSum(arr, S) {
+    let smallestSubarrayLength = Infinity; 
+
+    let windowStart = 0; 
+    let currentSum = 0; 
+
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        currentSum += arr[windowEnd]; 
+
+        while (currentSum >= S) {
+            if (windowEnd - windowStart + 1 < smallestSubarrayLength) {
+                smallestSubarrayLength = windowEnd - windowStart + 1;
+            }
+            currentSum -= arr[windowStart];
+            windowStart++;
+        }
+    }
+
+    if (smallestSubarrayLength === Infinity) {
+        return 0
+    } 
+
+    return smallestSubarrayLength
 }
 
-// console.log(smallestSubarrayWithGivenSum([2, 1, 5, 2, 3, 2], 7)) // returns 2
-// console.log(smallestSubarrayWithGivenSum([2, 1, 5, 2, 8], 7)) // returns 1
-// console.log(smallestSubarrayWithGivenSum([3, 4, 1, 1, 6], 8)) // returns 3
-// console.log(smallestSubarrayWithGivenSum([3, 4, 4, 1, 1, 5, 6], 11)) // returns 2
-// console.log(smallestSubarrayWithGivenSum([3, 4, 4, 1, 1, 5, 6], 14)) // returns 5
-// console.log(smallestSubarrayWithGivenSum([3, 4, 4, 1, 1, 5, 6], 140)) // returns 0
+console.log(smallestSubarrayWithGivenSum([2, 1, 5, 2, 3, 2], 7)) // returns 2
+console.log(smallestSubarrayWithGivenSum([2, 1, 5, 2, 8], 7)) // returns 1
+console.log(smallestSubarrayWithGivenSum([3, 4, 1, 1, 6], 8)) // returns 3
+console.log(smallestSubarrayWithGivenSum([3, 4, 4, 1, 1, 5, 6], 11)) // returns 2
+console.log(smallestSubarrayWithGivenSum([3, 4, 4, 1, 1, 5, 6], 14)) // returns 5
+console.log(smallestSubarrayWithGivenSum([3, 4, 4, 1, 1, 5, 6], 140)) // returns 0
