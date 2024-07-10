76c8b660a47e59238e8de56170949e998223030a
adrian <adrianadames@gmail.com>
2024-07-10 17:39:56 -0400

    Done with problems 6 and 7.

--- grokkingCodeInterview/2-slidingWindow/6-longestSubstringWithSameLettersAfterReplacement.js
+++ grokkingCodeInterview/2-slidingWindow/6-longestSubstringWithSameLettersAfterReplacement.js
@@ -21,13 +21,64 @@ Example 3:
     Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
 */
 
+// - keep a count of the most frequent appearing character in the window and then add k. that's the 
+// answer
 
+// time complexity: O(N);
+// space complexity: O(1);
 let longestSubstringWithSameLettersAfterReplacement = (str, k) => {
+    let longestSubstringLength = 0; 
+    let charTracker = {};
+    let countOfMostFreqCharInWindow = 0; 
+    let mostFreqCharInWindow = ''; 
+    let windowStart = 0;
 
+    for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+        if (!charTracker[str[windowEnd]]) {
+            charTracker[str[windowEnd]] = 0; 
+        }
+        charTracker[str[windowEnd]]++; 
+
+        // - this is the part i'm confused about; if we just added a letter that made it's
+        // frequency equal to that of the largest frequency letter, which one do we save
+        // as the highest frequency count since their count is both equal?wha
+        if (charTracker[str[windowEnd]] >= countOfMostFreqCharInWindow) {
+            countOfMostFreqCharInWindow = charTracker[str[windowEnd]]; 
+            mostFreqCharInWindow = str[windowEnd];
+        }
+
+        // - if adding the character to the window violates the condition for validity, we need to shrink the 
+        // window from the left until the condition is met
+        while (windowEnd - windowStart + 1 - countOfMostFreqCharInWindow > k) {
+            let leftChar = str[windowStart]; 
+
+            charTracker[str[windowStart]]--;
+            windowStart++; 
+
+            // - if the character we just removed was the mostFreqentCharInWindow, then we will need 
+            // to loop through our dictionary to update the new mostFrequentCharInWindow
+            if (leftChar === mostFreqCharInWindow) {
+                countOfMostFreqCharInWindow = 0; // - reset countOfMostFrequentCharInWindow 
+                for (let key in charTracker) {
+                    if (charTracker[key] >= countOfMostFreqCharInWindow) {
+                        countOfMostFreqCharInWindow = charTracker[key];
+                        mostFreqCharInWindow = key;  
+                    }
+                }
+            }
+        }
+        // - after the above while loop, the constraint should be satisfied; we then check if this is the 
+        // the longest substring we've encountered so far; 
+        if (windowEnd - windowStart + 1 > longestSubstringLength) {
+            longestSubstringLength = windowEnd - windowStart + 1;
+        }
+    }
+
+    return longestSubstringLength
 };
 
-// console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('aabccbb', 2)); 
-// console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abbcb', 1));
-// console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abccde',1));
+console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('aabccbb', 2)); 
+console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abbcb', 1));
+console.log('longestSubstringWithSameLettersAfterReplacement: ', longestSubstringWithSameLettersAfterReplacement('abccde',1));
 
 
--- grokkingCodeInterview/2-slidingWindow/7-longestSubstringWithOnesAfterReplacement.js
+++ grokkingCodeInterview/2-slidingWindow/7-longestSubstringWithOnesAfterReplacement.js
@@ -19,9 +19,41 @@ Example 2:
     longest contiguous subarray of 1s having length 9.
 */
 
+// time complexity: O(N); 
+// space complexity: O(1);
 function longestContinuousSubarrayOfAllOnes(arr, k) {
+    // - we use the sliding window method
+    // - as we expand the window, we need to make sure that the the following condition is met: 
+    // (windowEnd - windowStart + 1 - numberOfOnesInWindow) > k
+    
+    let numberOfOnesInWindow = 0; 
+    let longestSubarrayLength = 0; 
+    let windowStart = 0; 
 
+    for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+        // - if the number we're about to add is 0, we need to check if adding it violated our condition; 
+        if (arr[windowEnd] === 0) {
+            while (windowEnd - windowStart + 1 - numberOfOnesInWindow > k) {
+                // - if condition violated, shrink window from the left
+                if (arr[windowStart] === 1) {
+                    numberOfOnesInWindow--;
+                    windowStart++;
+                } else {
+                    windowStart++;
+                }
+            }
+        } else {
+            // - if the number we're about to add is a 1;
+            numberOfOnesInWindow++;
+        }
+        // - after adding the number at windowEnd and shrinking the window if need be
+        // we check if this length of the window is the longest encountered so far; 
+        if (windowEnd - windowStart + 1 > longestSubarrayLength) {
+            longestSubarrayLength = windowEnd - windowStart + 1;
+        }
+    }
+    return longestSubarrayLength
 };
 
-// console.log('longestContinuousSubarrayOfAllOnes: ', longestContinuousSubarrayOfAllOnes([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2));
-// console.log('longestContinuousSubarrayOfAllOnes: ', longestContinuousSubarrayOfAllOnes([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3));
+console.log('longestContinuousSubarrayOfAllOnes: ', longestContinuousSubarrayOfAllOnes([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2));
+console.log('longestContinuousSubarrayOfAllOnes: ', longestContinuousSubarrayOfAllOnes([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3));
