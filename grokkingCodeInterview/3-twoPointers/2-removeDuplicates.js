991868b8eaca67ffd7b654eca0652e66971fc3c9
adrian <adrianadames@gmail.com>
2024-07-19 17:39:50 -0400

    Done with problem 2.

--- grokkingCodeInterview/3-twoPointers/2-removeDuplicates.js
+++ grokkingCodeInterview/3-twoPointers/2-removeDuplicates.js
@@ -20,25 +20,34 @@ Example 2:
 
 // time: O(n) = n; 
 // space: O(n) = 1;
-let removeDuplicates = (arr) => {
-    let leftPointer = 0;
-    let rightPointer = 1;
+
+
+// - we put leftPointer at 0 and rightPointer at 1; 
+// - we hold the leftPointer at its value and move the rightPointer until we encounter a 
+// a distinct number; 
+// - once we encounter the distinct value, we increment leftPointer by 1, and swap 
+// values with the encountered distinct value; 
+// - we then increment rightPointer until we encounter another distinct number; 
+
+
+function removeDuplicates2(arr) {
+    let leftPointer = 0; 
+    let rightPointer = 1; 
 
     while (rightPointer < arr.length) {
-        // -once non-duplicate encountered, swap it with the number in front of the left
-        // pointer and increment both pointers
+        // - if the number at leftPointer is distinct from number at rightPointer, 
+        // then advance the leftPointer and switch its value with that at the rightPointer
         if (arr[leftPointer] !== arr[rightPointer]) {
-            [arr[leftPointer+1], arr[rightPointer]] = [arr[rightPointer], arr[leftPointer+1]];
-            leftPointer+=1;
-            rightPointer+=1;
+            leftPointer++;
+            [arr[leftPointer], arr[rightPointer]] = [arr[rightPointer], arr[leftPointer]];
+            rightPointer++;
         } else {
-            rightPointer+=1;
-        } 
+            rightPointer++;
+        }
     }
-    arr.splice(leftPointer+1);
-    // console.log('arr: ', arr);
     return leftPointer + 1
-}
+};
 
-console.log('removeDuplicates: ', removeDuplicates([2, 3, 3, 3, 6, 9, 9]));
-console.log('removeDuplicates: ', removeDuplicates([2, 2, 2, 11]));
+console.log('removeDuplicates2: ', removeDuplicates2([2, 3, 3, 3, 6, 9, 9]));
+console.log('removeDuplicates2: ', removeDuplicates2([2, 2, 2, 11]));
+
