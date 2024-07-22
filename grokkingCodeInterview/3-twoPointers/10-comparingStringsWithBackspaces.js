4f601477326ea0fe9022c1eb4df79ed6fd730e77
adrian <adrianadames@gmail.com>
2024-07-22 16:16:52 -0400

    Added problem statements for the remaining problems in this section.

---
+++ grokkingCodeInterview/3-twoPointers/10-comparingStringsWithBackspaces.js
@@ -0,0 +1,9 @@
+/*
+(medium)
+
+Problem - Comparing Strings containing Backspaces
+
+Given two strings containing backspaces (identified by the character ‘#’), 
+check if the two strings are equal.
+
+*/
---
+++ grokkingCodeInterview/3-twoPointers/11-minimumWindowSort.js
@@ -0,0 +1,9 @@
+/*
+(medium) 
+
+Problem - Minimum Window Sort 
+
+Given an array, find the length of the smallest subarray 
+in it which when sorted will sort the whole array.
+
+*/
