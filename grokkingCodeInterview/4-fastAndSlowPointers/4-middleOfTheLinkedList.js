79b419f6d43d6d651ed6d4206af160cf5c7868ed
adrian <adrianadames@gmail.com>
2024-08-03 01:46:15 -0400

    Done with final revision for problem 4.

--- grokkingCodeInterview/4-fastAndSlowPointers/4-middleOfTheLinkedList.js
+++ grokkingCodeInterview/4-fastAndSlowPointers/4-middleOfTheLinkedList.js
@@ -17,16 +17,20 @@ Example 3:
     Input: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7 -> null
     Output: 4
 */
+
 class Node {
     constructor(value, next = null) {
         this.value = value; 
         this.next = next;
     }
-}
+};
+
 function middleOfLinkedList(head) {
     let fast = head;
     let slow = head;
 
+    // - if fast === null there's an even number of nodes; 
+    // - if fast.next === null there's an odd number of nodes;
     while (fast !== null && fast.next !== null) {
         fast = fast.next.next;
         slow = slow.next;
@@ -42,4 +46,4 @@ head.next.next.next = new Node(4);
 head.next.next.next.next = new Node(5);
 head.next.next.next.next.next = new Node(6);
 
-console.log('middleOfLinkedList: ', middleOfLinkedList(head))
+console.log('middleOfLinkedList: ', middleOfLinkedList(head));
