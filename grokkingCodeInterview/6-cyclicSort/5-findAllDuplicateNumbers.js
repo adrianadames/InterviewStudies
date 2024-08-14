7622259faf87f42b66556319ddf5c9488a8c269a
adrian <adrianadames@gmail.com>
2024-08-14 15:37:03 -0400

    Attempted both problems. Got working solution, but I'm still confused about these problems.

--- grokkingCodeInterview/6-cyclicSort/5-findAllDuplicateNumbers.js
+++ grokkingCodeInterview/6-cyclicSort/5-findAllDuplicateNumbers.js
@@ -17,3 +17,58 @@ Example 2:
 Input: [5, 4, 7, 2, 3, 5, 3]
 Output: [3, 5]
 */
+
+function findAllDuplicateNumbers(arr) {
+    let i = 0; 
+    while (i < arr.length) {
+        let currentValue = arr[i]; 
+        let correctIndex = arr[i] - 1; 
+        let numberAtCorrectIndex = arr[correctIndex];
+
+        if (arr[i] !== arr[correctIndex] && arr[i] !== i +1) {
+            arr[i] = numberAtCorrectIndex;
+            arr[correctIndex] = currentValue;
+        } else {
+            i++;
+        }
+    }
+    // console.log('arr: ', arr)
+
+    let duplicates = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== i +1) {
+            duplicates.push(arr[i])
+        }
+    }
+    return duplicates
+}
+
+// - from grokking solutions
+function findAllDuplicateNumbers2(nums) {
+    const result = [];
+    const n = nums.length;
+    
+    for (let i = 0; i < n; i++) {
+        while (nums[i] !== i + 1) {
+            const correctIndex = nums[i] - 1;
+            if (nums[i] === nums[correctIndex]) {
+                // Duplicate found
+                if (result[result.length - 1] !== nums[i]) {
+                    result.push(nums[i]);
+                }
+                break;
+            }
+            // Swap
+            [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+        }
+    }
+    
+    return result;
+}
+
+console.log('findAllDuplicateNumbers: ', findAllDuplicateNumbers([3, 4, 3]));
+console.log('findAllDuplicateNumbers: ', findAllDuplicateNumbers([5, 4, 7, 2, 3, 5, 3]));
+
+
+
+
