01f8a4be83aae9dd49c518aa5818c2e92debe521
adrian <adrianadames@gmail.com>
2024-08-20 21:14:15 -0400

    Done with problems 1-5. Time and space complexity for every problem in this set is O(N) and O(1), respectively.

--- grokkingCodeInterview/7-inPlaceLinkedListReversal/2-reverseSubList.js
+++ grokkingCodeInterview/7-inPlaceLinkedListReversal/2-reverseSubList.js
@@ -11,3 +11,80 @@ original list: p = 2, q = 4
 head ->1 ->2 ->3 ->4 ->5 ->null
 head ->1 ->4 ->3 ->2 ->5 ->null
 */
+
+// - keep track of position on the linked list; 
+// - note: i'm going to make an assumption; I'm going to assume that the position 
+// indexing starts with the number 1 at the head; 
+
+class Node {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+
+function reverseSubList(head, p, q) {
+    if (p === q || head === null || head.next === null) {
+        return head
+    }
+
+    let position = 1;
+    let current = head;
+    let prev = null; 
+
+    // - skip the first p - 1 nodes
+    while (position < p) {
+        prev = current;
+        current = current.next;
+        position++;  
+    }
+    // - at this point, current is at position p and prev is at position p - 1; we need to  
+    // keep track of the node at prev because when we reverse p through q, we need the node 
+    // at q to be the next of this node
+    let frontNode = prev; 
+
+    // - we also keep track of the node at p; this node will the be last node in the reversed sublist
+    let lastNodeOfSublist = current; 
+
+    // - we reverse the sublist from p to q; 
+    while (position <= q) {
+        let next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+        position++;
+    }
+
+    // - at the end of the reversal, current is the node right after q (i.e. at the position q + 1), and no 
+    // nodes have it as the next node; we need to keep track of this node because this is what the node that 
+    // was originally at p needs to have as the next node
+    let backNode = current;
+
+    // - and prev is now the node at q which is now the head of the sublist we just reversed
+    if (frontNode !== null) {
+        frontNode.next = prev;
+    } else {
+        head = prev;
+    }    
+
+    lastNodeOfSublist.next = backNode
+
+    return head
+}
+
+// console.log('reverseSubList: ', reverseSubList(head, 1, 4));
+
+let node = reverseSubList(head, 2, 4);
+
+while (node) {
+    console.log('node: ', node.value);
+    node = node.next;
+}
+
+
--- grokkingCodeInterview/7-inPlaceLinkedListReversal/4-reverseAlternatingKElementSublist.js
+++ grokkingCodeInterview/7-inPlaceLinkedListReversal/4-reverseAlternatingKElementSublist.js
@@ -16,3 +16,76 @@ reversed sub-list:
 head ->2 ->1 ->3 ->4 ->6 ->5 ->7 ->8 ->null
 
 */
+class Node {
+    constructor(value, next = null) {
+        this.value = value; 
+        this.next = next;
+    }
+}
+
+let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+head.next.next.next.next.next.next = new Node(7);
+head.next.next.next.next.next.next.next = new Node(8);
+
+// - do reversal, traverse k nodes, repeat
+function reverseAlternatingKSizeSubList(head, k) {
+    if (head === null || head.next === null || k <= 0) {
+        return head;
+    }
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+        let position = 1;
+        let frontNode = prev;
+        let lastNodeOfReversedSublist = current;
+
+        while (position <= k && current !== null) {
+            let next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            position++;
+        }
+
+        if (frontNode !== null) {
+            frontNode.next = prev
+        } else {
+            head = prev
+        }
+
+        lastNodeOfReversedSublist.next = current;
+        prev = lastNodeOfReversedSublist;
+
+        for (let i = 0; i < k; i++) {
+            if (current === null) {
+                break;
+            } 
+            current = current.next;
+            prev = prev.next;
+        }
+
+    }
+    return head
+}
+
+let node = reverseAlternatingKSizeSubList(head, 2); 
+// console.log('node: ', node);
+// console.log('node.next.: ', node.next);
+// console.log('node.next.next: ', node.next.next);
+// console.log('node.next.next.next: ', node.next.next.next);
+// console.log('node.next.next.next.next: ', node.next.next.next.next);
+// console.log('node.next.next.next.next.next: ', node.next.next.next.next.next);
+// console.log('node.next.next.next.next.next.next: ', node.next.next.next.next.next.next);
+// console.log('node.next.next.next.next.next.next.next: ', node.next.next.next.next.next.next.next);
+
+
+while (node) {
+    console.log('node: ', node);
+    node = node.next;
+}
