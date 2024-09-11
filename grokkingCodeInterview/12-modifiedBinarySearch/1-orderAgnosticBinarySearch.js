4950cdee812953182efd2a0cf9cb11132106752b
adrian <adrianadames@gmail.com>
2024-09-11 17:59:57 -0400

    Done with problem 1.

--- grokkingCodeInterview/12-modifiedBinarySearch/1-orderAgnosticBinarySearch.js
+++ grokkingCodeInterview/12-modifiedBinarySearch/1-orderAgnosticBinarySearch.js
@@ -26,3 +26,40 @@ Example 4:
 Input: [10, 6, 4], key = 4
 Output: 2
 */
+
+function orderAgnosticBinarySearch(arr, key) {
+    let left = 0;
+    let right = arr.length -1;
+    let isAscending = arr[left] < arr[right];
+    while (left <= right) {
+        let middle = Math.floor((left + right)/2);
+
+        if (arr[middle] === key) {
+            return middle
+        } else if (isAscending) {
+            // - if middle element greater than key in ascending array, key is in
+            // left half of the array. move the right pointer to the element to 
+            // the left of the middle pointer (i.e. middle - 1)
+            if (arr[middle] > key) { 
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+            // - if middle element greater than key in descending array, key is in 
+            // right half of the array. move the left pointer to the element to 
+            // the left of the middle pointer (i.e. middle + 1)
+        } else {
+            if (arr[middle] > key) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            }
+        }
+    }
+    return -1
+}
+
+console.log(orderAgnosticBinarySearch([4, 6, 10], 10)); // 2
+console.log(orderAgnosticBinarySearch([1, 2, 3, 4, 5, 6, 7], 5)); // 4
+console.log(orderAgnosticBinarySearch([10, 6, 4], 10)); // 0 
+console.log(orderAgnosticBinarySearch([10, 6, 4], 4)); // 2
