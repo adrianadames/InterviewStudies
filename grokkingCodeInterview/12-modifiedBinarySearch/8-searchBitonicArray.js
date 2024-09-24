c3ab13c4cb17a7c7093ec7732bdfc28e174d6b6b
adrian <adrianadames@gmail.com>
2024-09-24 17:49:09 -0400

    Done with problems 7 and 8.

--- grokkingCodeInterview/12-modifiedBinarySearch/8-searchBitonicArray.js
+++ grokkingCodeInterview/12-modifiedBinarySearch/8-searchBitonicArray.js
@@ -26,3 +26,77 @@ Example 4:
 Input: [10, 9, 8], key=10
 Output: 0
 */
+
+// time: O(n) = log(n);
+// space: O(n) = 1;
+
+// - procedure: 
+// 1) find the max; 
+// 2) search for the key on both sides of the max; 
+
+function findBitonicArrayMax(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let middle = Math.floor((left + right) /2);
+        if (arr[middle] < arr[middle + 1]) {
+           left = middle + 1;  
+        } else {
+            right = middle;
+        };
+    };
+    return left; 
+};
+
+function searchBitonicArray(arr, key) {
+    let maxElementIndex = findBitonicArrayMax(arr);
+    let leftSideSearch = binarySearch(arr, 0, maxElementIndex, key);
+    let rightSideSearch = binarySearch(arr, maxElementIndex+1, arr.length-1, key);
+
+    if (leftSideSearch !== -1) {
+        return leftSideSearch;
+    } else if (rightSideSearch !== -1) {
+        return rightSideSearch;
+    } else {
+        return -1;
+    };
+};
+
+function binarySearch(arr, leftStart, rightStart, key) {
+    let left = leftStart; 
+    let right = rightStart;
+    let isAscending = arr[left] < arr[right];
+
+    while (left <= right) {
+        let middle = Math.floor((left + right)/2);
+        if (arr[middle] === key) {
+            return middle;
+        } else if (isAscending) {
+            // - if middle element greater than key in ascending array, key is in
+            // left half of the array. move the right pointer to the element to 
+            // the left of the middle pointer (i.e. middle - 1)
+            if (arr[middle] > key) { 
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            };
+            // - if middle element greater than key in descending array, key is in 
+            // right half of the array. move the left pointer to the element to 
+            // the left of the middle pointer (i.e. middle + 1)
+        } else {
+            if (arr[middle] > key) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
+            };
+        };
+    };
+
+    return -1;
+};
+
+console.log('searchBitonicArray: ', searchBitonicArray([1, 3, 8, 4, 3], 4));
+console.log('searchBitonicArray: ', searchBitonicArray([3, 8, 3, 1], 8));
+console.log('searchBitonicArray: ', searchBitonicArray([1, 3, 8, 12], 12));
+console.log('searchBitonicArray: ', searchBitonicArray([10, 9, 8], 10));
