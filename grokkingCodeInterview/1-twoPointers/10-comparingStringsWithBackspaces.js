9c14b56dc02d75612ec2a7a4e91d7ff4986264ec
adrian <adrianadames@gmail.com>
2024-10-16 13:58:03 -0400

    Done with problems 10 and 11.

--- grokkingCodeInterview/1-twoPointers/10-comparingStringsWithBackspaces.js
+++ grokkingCodeInterview/1-twoPointers/10-comparingStringsWithBackspaces.js
@@ -3,7 +3,80 @@
 
 Problem - Comparing Strings containing Backspaces
 
-Given two strings containing backspaces (identified by the character ‘#’), 
-check if the two strings are equal.
+Given two strings s and t, return true if they are equal when both are 
+typed into empty text editors. '#' means a backspace character.
 
+Note that after backspacing an empty text, the text will continue empty.
+
+Example 1:
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+
+Example 2:
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+
+Example 3:
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+ 
+Constraints:
+1 <= s.length, t.length <= 200
+s and t only contain lowercase letters and '#' characters.
+
+Follow up: Can you solve it in O(n) time and O(1) space?
 */
+
+// time: O(n)
+// space: O(1)
+function comparingStringsWithBackspaces(str1, str2) {
+    let p1 = str1.length-1;
+    let p2 = str2.length-1;
+
+    // - we put a pointer at the end of the string and then
+    // process all backspaces until we hit a letter
+    let backspaceCount1 = 0;
+    let backspaceCount2= 0;
+    while (p1 >=0 || p2 >=0) {
+        while (p1 >= 0) {
+            if (str1[p1] === '#') {
+                backspaceCount1++;
+                p1--;
+            } else if (backspaceCount1 > 0) {
+                p1--;
+            } else {
+                break
+            }
+        }
+
+        while (p2 >= 0) {
+            if (str2[p2] === '#') {
+                backspaceCount2++;
+                p2--;
+            } else if (backspaceCount2 > 0) {
+                p2--;
+            } else {
+                break
+            }
+        }
+
+        if (p1 <0 && p2 <0) {
+            return true
+        } else if ((p1 < 0 && p2>=0) || (p2 < 0 && p1 >= 0)) {
+            return false
+        } else if (str1[p1] !== str2[p2]) {
+            return false
+        } else { // str1[p1] === str2[p2]
+            p1--;
+            p2--;
+        }
+    }
+    return true;    
+}
+
+console.log('comparingStringsWithBackspaces: ', comparingStringsWithBackspaces("ab#c", "ad#c" ));
+console.log('comparingStringsWithBackspaces: ', comparingStringsWithBackspaces("ab##", "c#d#" ));
+console.log('comparingStringsWithBackspaces: ', comparingStringsWithBackspaces("a#c", "b" ));
--- grokkingCodeInterview/1-twoPointers/11-minimumWindowSort.js
+++ grokkingCodeInterview/1-twoPointers/11-minimumWindowSort.js
@@ -3,7 +3,69 @@
 
 Problem - Minimum Window Sort 
 
-Given an array, find the length of the smallest subarray 
-in it which when sorted will sort the whole array.
+Given an integer array nums, you need to find one continuous 
+subarray such that if you only sort this subarray in non-decreasing 
+order, then the whole array will be sorted in non-decreasing order.
 
+Return the shortest such subarray and output its length.
+
+Example 1:
+Input: nums = [2,6,4,8,10,9,15]
+Output: 5
+Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
+
+Example 2:
+Input: nums = [1,2,3,4]
+Output: 0
+
+Example 3:
+Input: nums = [1]
+Output: 0
+
+Constraints:
+1 <= nums.length <= 104
+-105 <= nums[i] <= 105
+
+Follow up: Can you solve it in O(n) time complexity?
 */
+// time: O(n)
+// space: O(1)
+function minWindowSort(arr) {
+    let left = 0;
+    let right = arr.length -1; 
+    
+    while (left < arr.length && arr[left] <= arr[left + 1]) {
+        left++;
+    }
+
+    if (left === arr.length -1) { // - array already sorted 
+        return 0; 
+    }
+
+    while (right > 0 && arr[right] >= arr[right - 1]) {
+        right--;
+    }
+
+    console.log('left: ', left);
+    console.log('right: ', right)
+
+    let min = Math.min(...arr.slice(left,right+1))
+    let max = Math.max(...arr.slice(left,right+1))
+    console.log('max: ', max);
+    console.log('min: ', min);
+
+    while (left > 0 && arr[left-1] > min) {
+        left--;
+    }
+
+    while (right < arr.length -1 && arr[right+1] < max) {
+        right++;
+    }
+
+    return [right - left + 1, arr.slice(left,right+1)];
+}
+
+console.log('minWindowSort: ', minWindowSort([2,6,4,8,10,9,15]));
+// console.log('minWindowSort: ', minWindowSort([1,2,3,4]));
+// console.log('minWindowSort: ', minWindowSort([1]));
+console.log('minWindowSort: ', minWindowSort([1,3,5,2,6,8,9]));
