cf247a41a2021aac1cfe3098561f4370301d0b86
adrian <adrianadames@gmail.com>
2024-10-20 16:28:25 -0400

    Done with problems 7 and 8.

--- grokkingCodeInterview/5-cyclicSort/7-findSmallestMissingPositive.js
+++ grokkingCodeInterview/5-cyclicSort/7-findSmallestMissingPositive.js
@@ -3,52 +3,59 @@
 
 Problem Statement: 
 
-Given an unsorted array containing numbers, find the 
-smallest missing positive number in it.
+Given an unsorted integer array nums. Return the smallest positive 
+integer that is not present in nums. You must implement an algorithm 
+that runs in O(n) time and uses O(1) auxiliary space.
 
 Example 1:
-
-Input: [-3, 1, 5, 4, 2]
+Input: nums = [1,2,0]
 Output: 3
-Explanation: The smallest missing positive number is '3'
+Explanation: The numbers in the range [1,2] are all in the array.
 
 Example 2:
-
-Input: [3, -2, 0, 1, 2]
-Output: 4
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
 
 Example 3:
-
-Input: [3, 2, 5, 1]
-Output: 4
+Input: nums = [7,8,9,11,12]
+Output: 1
+Explanation: The smallest positive integer 1 is missing.
 */
 
+// time: O(n);
+// space: O(1);
 function findSmallestMissingPositive(arr) {
     let i = 0; 
 
     while (i < arr.length) {
-        let currentValue = arr[i];
-        let correctIndex = arr[i] -1; 
-        let valueAtCorrectIndex = arr[correctIndex];
-
-        if (arr[i] > 0 && arr[i] !== arr[correctIndex]) {
-            arr[i] = valueAtCorrectIndex; 
-            arr[correctIndex] = currentValue;
-        } else {
+        let val = arr[i];
+        if (arr[i] > arr.length || arr[i] <= 0) {
+            i++
+        } else if (arr[val -1] !== val) {
+            arr[i] = arr[val -1];
+            arr[val -1] = val;
+        } else if (arr[val -1] === val) {
             i++;
         }
     }
-    console.log('arr: ', arr)
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== i+1) {
-            return i+1
+        if (arr[i] !== i + 1) {
+            return i + 1;
         }
     }
+    return arr.length + 1
 }
 
-console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([-3, 1, 5, 4, 2]))
-console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([3, -2, 0, 1, 2]))
-console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([3, 2, 5, 2]))
 
-// console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([3, 2, 5, 2]))
+console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([1,2,0]))
+console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([3,4,-1,1]))
+console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([7,8,9,11,12]))
+
+
+
+
+console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([-3, 1, 5, 4, 2]));
+console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([3, -2, 0, 1, 2]));
+console.log('findSmallestMissingPositive: ', findSmallestMissingPositive([3, 2, 5, 1]));
--- grokkingCodeInterview/5-cyclicSort/8-findFirstKMissingPositives.js
+++ grokkingCodeInterview/5-cyclicSort/8-findFirstKMissingPositives.js
@@ -24,3 +24,51 @@ Input: [-2, -3, 4], k=2
 Output: [1, 2]
 Explanation: The smallest missing positive numbers are 1 and 2.
 */
+
+// time: O(n + k)
+// space: O(k) for storing the k numbers
+function findFirstKMissingPositives(arr, k) {
+    let i = 0; 
+    while (i < arr.length) {
+        let val = arr[i];
+        if (arr[i] > arr.length || arr[i] <= 0) {
+            i++;
+        } else if (arr[val -1] !== val) {
+            arr[i] = arr[val -1];
+            arr[val -1] = val;
+        } else if (arr[val -1] === val) {
+            i++;
+        }
+    }
+
+    let missingPosNums = [];
+    let extras = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (missingPosNums.length < k) {
+            if (arr[i] !== i + 1) {
+                missingPosNums.push(i+1);
+                if (!extras[arr[i]]) {
+                    extras[arr[i]] = 0; 
+                } 
+                extras[arr[i]]++;
+            }
+        }
+    }
+
+    i = 1; 
+    while (missingPosNums.length < k) {
+        let next = arr.length + i;
+        if (!(extras[next])) {
+            missingPosNums.push(next);
+        }
+        i++;
+    }
+
+    return missingPosNums;
+}
+
+
+console.log('findFirstKMissingPositives: ', findFirstKMissingPositives([-3, 1, 5, 4, 2], 5)); // [3, 6, 7, 8, 9]
+console.log('findFirstKMissingPositives: ', findFirstKMissingPositives([3, -1, 4, 5, 5],3)); // [1, 2, 6]
+console.log('findFirstKMissingPositives: ', findFirstKMissingPositives([2, 3, 4], 3)); // [1, 5, 6]
+console.log('findFirstKMissingPositives: ', findFirstKMissingPositives([-2, -3, 4], 2)); // [1, 2]
