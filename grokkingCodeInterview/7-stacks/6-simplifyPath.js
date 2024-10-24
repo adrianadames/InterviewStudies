ce46b2caedc9dfa2a158091afd8fe1b851a7276c
adrian <adrianadames@gmail.com>
2024-10-24 16:08:57 -0400

    Done with problem 6.

--- grokkingCodeInterview/7-stacks/6-simplifyPath.js
+++ grokkingCodeInterview/7-stacks/6-simplifyPath.js
@@ -10,7 +10,7 @@ should represent the shortest absolute path.
 Example 1 
 Input: "/a//b////c/d//././/.."
 Output: "/a/b/c"
-   
+
 Example 2 
 Input: "/../"
 Output: "/"
@@ -24,3 +24,65 @@ Constraints:
 path consists of English letters, digits, period '.', slash '/' or '_'.
 path is a valid absolute Unix path.
 */
+
+// time: O(n);
+// space: O(n);
+function simplifyPath(pathStr) {
+    let stack = [];
+    let currentDir = '';
+    
+    for (let i = 0; i < pathStr.length; i++) {
+        let nextChar = pathStr[i];
+        if (nextChar === '/') {
+            if (currentDir === '..') {
+                if (stack.length > 0) {
+                    stack.pop();
+                }
+            } else if (currentDir !== '' && currentDir !== '.') {
+                stack.push(currentDir);
+            }
+            currentDir = '';
+        } else {
+            currentDir += nextChar;
+        }
+    }
+
+    if (currentDir === '..') {
+        if (stack.length > 0) {
+            stack.pop();
+        }
+    } else if (currentDir !== '' && currentDir !== '.') {
+        stack.push(currentDir);
+    }
+
+    return '/' + stack.join('/');
+}
+
+console.log('simplifyPath: ', simplifyPath("/a//b///../c/d//././/.."));
+console.log('simplifyPath: ', simplifyPath("/../"));
+console.log('simplifyPath: ', simplifyPath("/home//foo/"));
+
+// time: O(n);
+// space: O(n);
+function simplifyPath2(pathStr) {
+    let pathStrArray = pathStr.split('/');
+    let stack = [];
+
+    for (let i = 0; i < pathStrArray.length; i++) {
+        if (pathStrArray[i] === '' || pathStrArray[i] === '.') {
+            continue;
+        } else if (pathStrArray[i] === '..') {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else {
+            stack.push(pathStrArray[i]);
+        }
+    }
+
+    return '/' + stack.join('/');
+}
+
+console.log('simplifyPath2: ', simplifyPath2("/a//b////c/d//././/.."));
+console.log('simplifyPath2: ', simplifyPath2("/../"));
+console.log('simplifyPath2: ', simplifyPath2("/home//foo/"));
