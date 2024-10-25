777eeb7055b5cdd9ddc9b0ef0e0b14f17920cba3
adrian <adrianadames@gmail.com>
2024-10-25 18:30:22 -0400

    Done with problems 1-3 and problem 5. Current solution for problem 4 is off. Tabling this problem for now.

--- grokkingCodeInterview/9-hashMaps/4-longestPalindrome.js
+++ grokkingCodeInterview/9-hashMaps/4-longestPalindrome.js
@@ -25,3 +25,52 @@ Constraints:
 1 <= s.length <= 2000
 s consists of lowercase and/or uppercase English letters only.
 */
+
+// NOTE: THIS SOLUTION IS WRONG BECAUSE IT DOESN"T TAKE INTO ACCOUNT THAT 
+// AN ODD NUMBER COUNT LIKE {'a': 5} can be used to construct palindrome
+// by using four of the five letters;
+// ALSO not sure about the letter case thing. I think I have it taken care of 
+// because of how they are distinct when serving as object keys 
+function longestPalindrome(str) {
+    let charCount = {};
+    for (let i = 0; i < str.length; i++) {
+        if (!charCount[str[i]]) {
+            charCount[str[i]] = 0;
+        }
+        charCount[str[i]]++;
+    }
+
+    let largestOdd = ['', 0];
+    let result = [];
+    // - build (most of) the first half of the palindrome
+    Object.entries(charCount).forEach(keyValuePair => {
+        if (keyValuePair[1] % 2 === 0) {
+            for (let i = 0; i < keyValuePair[1]/2; i++) {
+                result.push(keyValuePair[0]);
+            }
+        } else {
+            if (keyValuePair[1] > largestOdd[1]) {
+                largestOdd[0] = keyValuePair[0];
+                largestOdd[1] = keyValuePair[1];
+            }
+        }
+    })
+
+    let firstHalfLength = result.length;
+    
+    // - add the letter with the largest odd count; 
+    for (let i = 0; i < largestOdd[1]; i++) {
+        result.push(largestOdd[0]);
+    };
+
+    // - reverse the original first half and add it to the result
+    for (let i = firstHalfLength - 1; i >= 0; i--) {
+        result.push(result[i]);
+    }
+
+    return result.join('');
+}
+console.log('longestPalindrome: ', longestPalindrome('abccccdd'));
+console.log('longestPalindrome: ', longestPalindrome('a'));
+console.log('longestPalindrome: ', longestPalindrome('abcb'));
+console.log('longestPalindrome: ', longestPalindrome('ballloommoolllab'));
