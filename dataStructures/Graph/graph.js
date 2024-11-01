c8f85b0f815db8b5b45415995d843cad947f58c7
adrian <adrianadames@gmail.com>
2024-11-01 17:34:07 -0400

    Revised Graph data structure. Added comments and fixed issues with dfsRecursivePath and dfsStackPath.

--- dataStructures/Graph/graph.js
+++ dataStructures/Graph/graph.js
@@ -4,21 +4,21 @@ NOTE:
 -There are different ways to implement the graph data structure. Implement another version. 
 
 Data structures to use in implementation of graph data structure: 
--Queue
--Stack
--Vertex
--Graph
+- [x] Queue
+- [x] Stack
+- [x] Vertex
+- [x] Graph
 
 Methods: 
--addVertex
--addEdge
--addDirectedEdge
--dftRecursive
--dftStack
--dfsRecursivePath
--dfsStackPath
--bft
--bfsPath
+- [x] addVertex
+- [x] addEdge
+- [x] addDirectedEdge
+- [x] dftRecursive
+- [x] dfsRecursivePath
+- [x] dftStack
+- [x] dfsStackPath
+- [x] bft
+- [x] bfsPath
 
 NOTE: 
 */
@@ -31,9 +31,9 @@ class Queue {
 
     enqueue(item) {
         if (item === null) {
-            return Error('The item passed into this function must be a non-null value.')
+            return Error('The item passed into this function must be a non-null value.');
         } else if (item === undefined) {
-            return Error('The item passed into the function is undefined.')
+            return Error('The item passed into the function is undefined.');
         } else {
             this.items.push(item);
             this.size = this.items.length;
@@ -42,7 +42,7 @@ class Queue {
 
     dequeue() {
         if (this.items.length < 1) {
-            return 'queue is empty'
+            return 'queue is empty';
         } else {
             this.size = this.items.length -1;
             return this.items.shift();
@@ -58,9 +58,9 @@ class Stack {
 
     push(item) {
         if (item === null) {
-            return Error('The item passed into this function must be a non-null value.')
+            return Error('The item passed into this function must be a non-null value.');
         } else if (item === undefined) {
-            return Error('The item passed into the function is undefined.')
+            return Error('The item passed into the function is undefined.');
         } else {
             this.items.push(item);
             this.size = this.items.length;
@@ -69,7 +69,7 @@ class Stack {
 
     pop() {
         if (this.items.length < 1) {
-            return 'stack is empty'
+            return 'stack is empty';
         } else {
             this.size = this.items.length-1;
             return this.items.pop();
@@ -79,15 +79,20 @@ class Stack {
 
 class Vertex {
     constructor(vertex_id, value = null, color = "white") {
-        this.id = vertex_id;  //unique id for each vertex
+        this.id = vertex_id;  // - unique id for each vertex
         this.value = value;
         this.color = color;
+        // - Q: pros and cons of using an object instead of a set here; 
         this.edges = new Set();
     }
 }
 
 class Graph {
-    // Graph to be represented as an object of vertices mapping each vertex to its edges
+    // - Graph represented as an object (called vertices) mapping each vertex id 
+    // (the key) to its respective instantiated vertex class representation (the 
+    // key value);
+    // - Q: What are the pros and cons of using a Set data structure here instead 
+    // of an object?
     constructor(vertices = {}) {
         this.vertices = vertices;
     }
@@ -101,7 +106,7 @@ class Graph {
             this.vertices[v1_id].edges.add(v2_id);
             this.vertices[v2_id].edges.add(v1_id);
         } else {
-            return ReferenceError('v1 or v2 is not a vertex in this graph')
+            return ReferenceError('v1 or v2 is not a vertex in this graph');
         }
     }
    
@@ -109,22 +114,45 @@ class Graph {
         if (this.vertices[v1_id] && this.vertices[v2_id]) {
             this.vertices[v1_id].edges.add(v2_id);
         } else {
-            return ReferenceError('v1 or v2 is not a vertex in this graph')
+            return ReferenceError('v1 or v2 is not a vertex in this graph');
         }
     }
 
     dftRecursive(start_vert_id, visited = []) {
         // 1) push start_vert to visited
-        // 2) for each start_vert edge, if it hasn't been visited, run dftRecursive on that
-
-        visited.push(start_vert_id);
-
+        // 2) for each start_vert edge, if it hasn't been visited, run 
+        // dftRecursive on that
+        visited.push(start_vert_id); 
         for (let edge of this.vertices[start_vert_id].edges) {
             if (!visited.includes(edge)) {
                 this.dftRecursive(edge, visited);
             }
         }
-        return visited
+        return visited;
+    } 
+    
+    dfsRecursivePath(start_vert_id, target_vert_id, visited = [], path = []) {
+        visited.push(start_vert_id);
+        
+        // - spread operator used so that each call of dfsRecursivePath
+        // gets the version of path that was being built up when it was called 
+        // (versus visited above which is shared across calls)
+        // - note: chatgpt says we can also just pop it off when we backtrack...
+        path = [...path, start_vert_id];
+
+        if (start_vert_id === target_vert_id) {
+            console.log('target found: ');
+            return path;
+        }
+        for (let edge of this.vertices[start_vert_id].edges) {
+            if (!visited.includes(edge)) {
+                let newPath = this.dfsRecursivePath(edge, target_vert_id, visited, path);
+                if (newPath) {
+                    return newPath;
+                }
+            }
+        }
+        return null;        
     } 
 
     dftStack(start_vert_id) {
@@ -133,57 +161,36 @@ class Graph {
         stack.push(start_vert_id);
 
         while (stack.size > 0) {
-            let currentVertId = stack.pop();
-            if (!visited.includes(currentVertId)) {
-                for (let edge of this.vertices[currentVertId].edges) {
+            let current_vert_id = stack.pop();
+            if (!visited.includes(current_vert_id)) {
+                visited.push(current_vert_id);
+                for (let edge of this.vertices[current_vert_id].edges) {
                     stack.push(edge);    
                 }
-                visited.push(currentVertId); 
             }
         }
-        return visited
+        return visited;
     }
 
-    // QQQQQQQQQQQQQQQ: Do I need to use path as a parameter for this algorithm? Does visited on its own suffice?
-    // I'm getting the same answer either way. Will revisit at a later time. 
-    dfsRecursivePath(start_vert_id, target_vert_id, visited = [], path = []) {
-        visited.push(start_vert_id);
-        path.push(start_vert_id);
-
-        if (start_vert_id === target_vert_id) {
-            console.log('visited: ', visited);
-            return path // OR return visited??????????
-        }
-        for (let edge of this.vertices[start_vert_id].edges) {
-            if (!visited.includes(edge)) {
-                let newPath = this.dfsRecursivePath(edge, target_vert_id, visited, path);
-                if (newPath) {
-                    return newPath
-                }
-            }
-        }
-        return null        
-    } 
-
-    // QQQQQQQQQQQQQQQ: Do I need to use path as a parameter for this algorithm? I don't think so. 
     dfsStackPath(start_vert_id, target_vert_id) {
         let stack = new Stack();
         let visited = [];
-        stack.push(start_vert_id);
+        stack.push([start_vert_id, [start_vert_id]]); // arr[0]: start_vert_id, arr[1]: [associated path]
 
-        while (stack.size>0) {
-            let currentVertId = stack.pop();
-            if (!visited.includes(currentVertId)) {
-                visited.push(currentVertId);
-                if (currentVertId === target_vert_id) {
-                    return visited
-                }
-                for (let edge of this.vertices[currentVertId].edges) {
-                    stack.push(edge);
+        while (stack.size > 0) {
+            let [current_vert_id, path] = stack.pop();
+            if (current_vert_id === target_vert_id) {
+                return path;
+            }
+            if (!visited.includes(current_vert_id)) {
+                visited.push(current_vert_id);
+                for (let edge of this.vertices[current_vert_id].edges) {
+                    // Q: should I add conditition to ignore edge if already in visited?
+                    stack.push([edge, [...path, edge]]);
                 }
             }
         }
-        return null
+        return null;
     }
     
     bft(start_vert_id) {
@@ -200,11 +207,11 @@ class Graph {
                 }
             }
         }
-        return visited
+        return visited;
     }
 
     bfsPath(start_vert_id, target_vert_id) {
-        let visited = []
+        let visited = [];
         let queue = new Queue();
         queue.enqueue([start_vert_id]);
 
@@ -218,17 +225,17 @@ class Graph {
                     let visitedVertices = visited.slice();
                     visitedVertices.push(dequeuedVertId); 
                     console.log('visitedVertices: ', visitedVertices);
-                    return dequeuedPath
+                    return dequeuedPath;
                 } 
                 visited.push(dequeuedVertId);
                 for (let edge of this.vertices[dequeuedVertId].edges) {
-                    let newPath = dequeuedPath.slicbfe();
+                    let newPath = dequeuedPath.slice();
                     newPath.push(edge);
                     queue.enqueue(newPath);
                 }
             }
         }
-        return null
+        return null;
     } 
 }
 
@@ -253,7 +260,11 @@ g1.addEdge(3,7);
 g1.addEdge(3,6);
 g1.addEdge(7,9);
 
-// console.log(g1.dftRecursive(0))
-// console.log(g1.dftStack(0))
+console.log('g1: ', g1);
 
-console.log(g1.bfsPath(0,7))
+// // NOTE: all below return expected values
+// console.log('g1.dftRecursive(0): ', g1.dftRecursive(0));
+// console.log('g1.dfsRecursivePath(0,9): ', g1.dfsRecursivePath(0,9));
+// console.log('g1.dfsStackPath(0,9): ', g1.dfsStackPath(0,9))
+// console.log(g1.bfsPath(0,7))
+// console.log(g1.bft(0))
