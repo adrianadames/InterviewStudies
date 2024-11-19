816e4bbb6932a678f36e5b5d90240bb235f15779
adrian <adrianadames@gmail.com>
2024-11-19 13:44:17 -0500

    Did first pass through problems. They all need to be revised.

--- grokkingCodeInterview/12-graphs/2-numOfProvinces.js
+++ grokkingCodeInterview/12-graphs/2-numOfProvinces.js
@@ -1,27 +1,27 @@
 /*
 (medium)
 (https://leetcode.com/problems/number-of-provinces/description/)
-There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
 
-A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+There are n cities. Some of them are connected, while some are not. If city 
+a is connected directly with city b, and city b is connected directly with 
+city c, then city a is connected indirectly with city c.
 
-You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+A province is a group of directly or indirectly connected cities and no other 
+cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the 
+ith city and the jth city are directly connected, and isConnected[i][j] = 0 
+otherwise.
 
 Return the total number of provinces.
 
- 
-
 Example 1:
-
-
 Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
 Output: 2
+
 Example 2:
-
-
 Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
 Output: 3
- 
 
 Constraints:
 
@@ -32,3 +32,31 @@ isConnected[i][j] is 1 or 0.
 isConnected[i][i] == 1
 isConnected[i][j] == isConnected[j][i]
 */
+
+function numOfProvinces(isConnected) {
+    let n = isConnected.length;
+    let visited = new Array(n).fill(false);
+    let provinceCount = 0;
+
+    const dfs = (city) => {
+        visited[city] = true;
+        for (let neighbor = 0; neighbor < n; neighbor++) {
+            if (isConnected[city][neighbor] === 1 && !visited[neighbor]) {
+                dfs(neighbor);
+            }
+        }
+    };
+    // console.log('visited: ', visited);
+
+    for (let i = 0; i < n; i++) {
+        if (!visited[i]) {
+            provinceCount++;
+            dfs(i);
+        }
+    }
+    // console.log('provinceCount: ', provinceCount);
+    return provinceCount;
+}
+
+console.log('numOfProvinces([[1,1,0],[1,1,0],[0,0,1]]): ', numOfProvinces([[1,1,0],[1,1,0],[0,0,1]]));
+console.log('numOfProvinces([[1,0,0],[0,1,0],[0,0,1]]): ', numOfProvinces([[1,0,0],[0,1,0],[0,0,1]]));
